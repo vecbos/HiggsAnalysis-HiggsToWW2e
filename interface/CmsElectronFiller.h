ae31892609cad4f59f44e4005806c53abc893f62
emanuele <>
2007-09-22 10:35:08 +0000

    specialized class for elcetron variables filling

---
+++ interface/CmsElectronFiller.h
@@ -0,0 +1,132 @@
+// -*- C++ -*-
+//-----------------------------------------------------------------------
+//
+// Package:    
+//      HiggsAnalysis/HiggsToWW2e
+// Description:
+//      Class CmsElectronFiller
+//      Simple class for dumping RECO (or AOD) contents to a ROOT tree
+//      
+// Original Author:  Emanuele Di Marco
+//         Created:  Fri Apr  6 18:05:34 CEST 2007
+//
+//-----------------------------------------------------------------------
+
+#ifndef CmsElectronFiller_h
+#define CmsElectronFiller_h
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/METReco/interface/CaloMET.h"
+#include "DataFormats/METReco/interface/CaloMETCollection.h"
+#include "DataFormats/METReco/interface/GenMET.h"
+#include "DataFormats/METReco/interface/GenMETCollection.h"
+
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsTree.h"
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsCandidateFiller.h"
+#include "CLHEP/HepMC/GenEvent.h"
+#include <TTree.h>
+
+using namespace cms;
+using namespace edm;
+using namespace reco;
+
+struct CmsElectronFillerData : public CmsCandidateFillerData {
+
+  // All the vectors that will store the stuff
+  // going into the tree.
+
+  vector<float> *pxAtInner, *pyAtInner, *pzAtInner, *xAtInner, *yAtInner, *zAtInner;
+  vector<float> *pxAtOuter, *pyAtOuter, *pzAtOuter, *xAtOuter, *yAtOuter, *zAtOuter;
+
+  vector<float> *ecal, *eraw, *caloEta, *caloPhi;
+  vector<int> *nClu, *nCry;
+
+  vector<float> *e2x2, *e3x3, *e5x5;
+  vector<float> *eMax, *e2nd;
+  vector<float> *s1s9, *s9s25;
+  vector<float> *covEtaEta, *covEtaPhi, *covPhiPhi;
+  vector<float> *lat, *phiLat, *etaLat, *a20, *a42;
+  
+  int *ncand;
+
+public:
+  void initialise();
+  void clearTrkVectors();
+
+
+};
+
+class CmsElectronFiller : public CmsCandidateFiller {
+
+ public:
+
+  // Constructors
+
+  // Dump everything
+  CmsElectronFiller(CmsTree *, int maxTracks=500,
+		int maxMCTracks=2000, bool noOutputIfLimitsReached=false );
+
+  // Dump  everything if fatTree is true and less informations otherwise
+  CmsElectronFiller(CmsTree *, bool fatTree, int maxTracks=500,
+		int maxMCTracks=2000, bool noOutputIfLimitsReached=false );
+
+
+  // Destructor
+  virtual ~CmsElectronFiller();
+
+  // Modifiers
+
+  void saveTrk(bool );
+  void saveEcal(bool );
+  void saveFatTrk(bool );
+  void saveFatEcal(bool );
+  void saveEleID(bool );
+
+  // Operators
+
+  // run number and all of that --- to implement
+
+  void writeCollectionToTree(const CandidateCollection *,
+			     const edm::Event&, const edm::EventSetup&,
+			     const std::string &columnPrefix, const std::string &columnSuffix,
+			     bool dumpData=false);
+
+ private:
+  
+  void writeTrkInfo(const Candidate *cand, const edm::Event&, const edm::EventSetup&, GsfTrackRef trkRef);
+  void treeTrkInfo(const std::string &colPrefix, const std::string &colSuffix);
+
+  void writeEcalInfo(const Candidate *cand, const edm::Event&, const edm::EventSetup&, SuperClusterRef);
+  void treeEcalInfo(const std::string &colPrefix, const std::string &colSuffix);
+
+
+  // Friends
+  bool saveTrk_;
+  bool saveEcal_;
+  bool saveFatTrk_;
+  bool saveFatEcal_;
+  bool saveEleID_;
+
+  bool hitLimitsMeansNoOutput_;
+  int maxTracks_;
+  int maxMCTracks_;
+
+  std::string *trkIndexName_;
+
+  CmsElectronFillerData *privateData_;
+  edm::InputTag matchMap_;
+
+  CmsTree *cmstree;
+
+
+};
+
+#endif // CmsElectronFiller_h
---
+++ src/CmsElectronFiller.cc
@@ -0,0 +1,492 @@
+//---------------------------------------------------------------------------
+//
+// Description:
+//       Package:   HtoWWTreeDumper
+//       Class:     CmsTreeFiller
+//
+// Original Author:  Emanuele Di Marco
+//         Created:  Mon Apr  9 11:01:00 CEST 2007
+//-----------------------------------------------------------------------
+
+// system include files
+#include <memory>
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "DataFormats/METReco/interface/CaloMET.h"
+#include "DataFormats/METReco/interface/CaloMETCollection.h"
+#include "DataFormats/METReco/interface/GenMET.h"
+#include "DataFormats/METReco/interface/GenMETCollection.h"
+
+#include "DataFormats/JetReco/interface/CaloJet.h"
+#include "DataFormats/JetReco/interface/CaloJetCollection.h"
+#include "DataFormats/JetReco/interface/GenJet.h"
+
+#include "DataFormats/RecoCandidate/interface/RecoCandidate.h"
+#include "DataFormats/RecoCandidate/interface/RecoEcalCandidate.h"
+
+#include "DataFormats/TrackReco/interface/Track.h"
+
+#include "DataFormats/EgammaReco/interface/BasicCluster.h"
+#include "DataFormats/EgammaReco/interface/SuperCluster.h"
+#include "DataFormats/EgammaReco/interface/BasicClusterShapeAssociation.h"
+#include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectron.h"
+#include "DataFormats/EgammaCandidates/interface/PixelMatchElectron.h"
+#include "DataFormats/EgammaCandidates/interface/PixelMatchElectronFwd.h"
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/Candidate/interface/CandMatchMap.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticleCandidate.h"
+
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsTree.h"
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsEleIDTreeFiller.h"
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsCandidateFiller.h"
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsElectronFiller.h"
+
+#include "PhysicsTools/HepMCCandAlgos/interface/MCCandMatcher.h"
+#include "CLHEP/HepMC/GenParticle.h"
+
+
+#include <TTree.h>
+
+#include <string>
+
+using namespace edm;
+using namespace reco;
+
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+//----------------
+// Constructors --
+//----------------
+
+
+CmsElectronFiller::CmsElectronFiller(CmsTree *cmsTree, 
+				     int maxTracks, int maxMCTracks,
+				     bool noOutputIfLimitsReached):
+  CmsCandidateFiller(cmsTree,maxTracks,maxMCTracks,noOutputIfLimitsReached),
+  privateData_(new CmsElectronFillerData)
+{
+  cmstree=cmsTree;
+
+  saveTrk_=true;
+  saveEcal_=true;
+  saveFatTrk_=true;
+  saveFatEcal_=true;
+
+  trkIndexName_ = new std::string("n");
+
+  hitLimitsMeansNoOutput_ = noOutputIfLimitsReached;
+  maxTracks_=maxTracks;
+  maxMCTracks_=maxMCTracks;
+
+  privateData_->initialise();
+}
+
+CmsElectronFiller::CmsElectronFiller(CmsTree *cmsTree, bool fatTree, 
+				     int maxTracks, int maxMCTracks,
+				     bool noOutputIfLimitsReached):
+  CmsCandidateFiller(cmsTree,maxTracks,fatTree,maxMCTracks,noOutputIfLimitsReached),
+  privateData_(new CmsElectronFillerData)
+{
+  cmstree=cmsTree;
+
+  saveTrk_=true;
+  saveEcal_=true;
+  saveFatTrk_=fatTree;
+  saveFatEcal_=fatTree;
+
+  trkIndexName_ = new std::string("n");
+
+  hitLimitsMeansNoOutput_ = noOutputIfLimitsReached;
+  maxTracks_=maxTracks;
+  maxMCTracks_=maxMCTracks;
+
+  privateData_->initialise();
+
+}
+
+
+//--------------
+// Destructor --
+//--------------
+
+CmsElectronFiller::~CmsElectronFiller() {
+
+  // delete here the vector ptr's
+  delete privateData_->pxAtInner;
+  delete privateData_->pyAtInner;
+  delete privateData_->pzAtInner;
+  delete privateData_->xAtInner;
+  delete privateData_->yAtInner;
+  delete privateData_->zAtInner;
+  delete privateData_->pxAtOuter;
+  delete privateData_->pyAtOuter;
+  delete privateData_->pzAtOuter;
+  delete privateData_->xAtOuter;
+  delete privateData_->yAtOuter;
+  delete privateData_->zAtOuter;
+
+  delete privateData_->ecal;
+  delete privateData_->eraw;
+  delete privateData_->caloEta;
+  delete privateData_->caloPhi;
+  delete privateData_->nClu;
+  delete privateData_->nCry;
+  delete privateData_->e2x2;
+  delete privateData_->e3x3;
+  delete privateData_->e5x5;
+  delete privateData_->eMax;
+  delete privateData_->e2nd;
+  delete privateData_->s1s9;
+  delete privateData_->s9s25;
+  delete privateData_->covEtaEta;
+  delete privateData_->covEtaPhi;
+  delete privateData_->covPhiPhi;
+  delete privateData_->lat;
+  delete privateData_->phiLat;
+  delete privateData_->etaLat;
+  delete privateData_->a20;
+  delete privateData_->a42;
+
+}
+
+
+//-------------
+// Methods   --
+//-------------
+
+// Set boolean control options for quantities that are written out
+
+void CmsElectronFiller::saveTrk(bool what) { saveTrk_=what;}
+
+void CmsElectronFiller::saveEcal(bool what) { saveEcal_=what;}
+
+void CmsElectronFiller::saveFatTrk(bool what) { saveFatTrk_=what;}
+
+void CmsElectronFiller::saveFatEcal(bool what) { saveFatEcal_=what;}
+
+void CmsElectronFiller::saveEleID(bool what) { saveEleID_=what;}
+
+void CmsElectronFiller::writeCollectionToTree(const CandidateCollection *collection,
+					      const edm::Event& iEvent, const edm::EventSetup& iSetup,
+					      const std::string &columnPrefix, const std::string &columnSuffix,
+					      bool dumpData) {
+  
+  if(hitLimitsMeansNoOutput_ && 
+     (int)collection->size() > maxTracks_){
+    LogError("CmsElectronFiller") << "Track length " << collection->size() 
+				  << " is too long for declared max length for tree "
+				  << maxTracks_ << " and no output flag is set."
+				  << " No tracks written to tuple for this event ";
+    return;
+  }
+  
+  if((int)collection->size() > maxTracks_){
+    LogError("CmsElectronFiller") << "Track length " << collection->size() 
+				  << " is too long for declared max length for tree "
+				  << maxTracks_ 
+				  << ". Collection will be truncated ";
+  }
+  
+  *(privateData_->ncand) = collection->size();
+
+  LogDebug("CmsElectronFiller") << "Filling candidate vectors";
+  privateData_->clearTrkVectors();
+  CandidateCollection::const_iterator cand;
+  for(cand=collection->begin(); cand!=collection->end(); cand++) {
+    // fill basic kinematics
+    if(saveCand_) writeCandInfo(&(*cand),iEvent,iSetup);
+    // fill Cluster Adapter
+    SuperClusterRef sclusRef = cand->get<SuperClusterRef>();
+    if(saveEcal_) writeEcalInfo(&(*cand),iEvent,iSetup,sclusRef);
+    // fill (GSF) Track Adapter
+    GsfTrackRef trkRef = cand->get<GsfTrackRef>();
+    if(saveTrk_) writeTrkInfo(&(*cand),iEvent,iSetup,trkRef);
+  }
+  
+  // The class member vectors containing the relevant quantities 
+  // have all been filled. Now transfer those we want into the 
+  // tree 
+  
+  std::string nCandString = columnPrefix+(*trkIndexName_)+columnSuffix; 
+  cmstree->column(nCandString.c_str(),collection->size(),0,"Reco");
+  
+  if(saveCand_) treeCandInfo(columnPrefix,columnSuffix);
+  if(saveEcal_) treeEcalInfo(columnPrefix,columnSuffix);
+  if(saveTrk_) treeTrkInfo(columnPrefix,columnSuffix);
+  if(saveEleID_) {
+    CmsEleIDTreeFiller eIDFiller(cmstree);
+    eIDFiller.setStandalone(false);
+    eIDFiller.writeCollectionToTree(collection,iEvent,iSetup,columnPrefix,columnSuffix,false);
+  }
+  
+  if(dumpData) cmstree->dumpData();
+
+}
+
+void CmsElectronFiller::writeTrkInfo(const Candidate *cand, const edm::Event& iEvent, const edm::EventSetup& iSetup, GsfTrackRef trkRef) {
+  if(&trkRef) {
+
+    privateData_->pxAtInner->push_back(trkRef->innerMomentum().x());
+    privateData_->pyAtInner->push_back(trkRef->innerMomentum().y());
+    privateData_->pzAtInner->push_back(trkRef->innerMomentum().z());
+
+    privateData_->xAtInner->push_back(trkRef->innerPosition().x());
+    privateData_->yAtInner->push_back(trkRef->innerPosition().y());
+    privateData_->zAtInner->push_back(trkRef->innerPosition().z());
+
+    privateData_->pxAtOuter->push_back(trkRef->outerMomentum().x());
+    privateData_->pyAtOuter->push_back(trkRef->outerMomentum().y());
+    privateData_->pzAtOuter->push_back(trkRef->outerMomentum().z());
+
+    privateData_->xAtOuter->push_back(trkRef->outerPosition().x());
+    privateData_->yAtOuter->push_back(trkRef->outerPosition().y());
+    privateData_->zAtOuter->push_back(trkRef->outerPosition().z());
+
+  }
+  else {
+    privateData_->pxAtInner->push_back(-1.);
+    privateData_->pyAtInner->push_back(-1.);
+    privateData_->pzAtInner->push_back(-1.);
+
+    privateData_->xAtInner->push_back(-1.);
+    privateData_->yAtInner->push_back(-1.);
+    privateData_->zAtInner->push_back(-1.);
+
+    privateData_->pxAtOuter->push_back(-1.);
+    privateData_->pyAtOuter->push_back(-1.);
+    privateData_->pzAtOuter->push_back(-1.);
+
+    privateData_->xAtOuter->push_back(-1.);
+    privateData_->yAtOuter->push_back(-1.);
+    privateData_->zAtOuter->push_back(-1.);
+
+  }
+}
+
+void CmsElectronFiller::treeTrkInfo(const std::string &colPrefix, const std::string &colSuffix) {
+  std::string nCandString=colPrefix+(*trkIndexName_)+colSuffix;
+
+  cmstree->column((colPrefix+"pxAtOuter"+colSuffix).c_str(), *privateData_->pxAtOuter, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"pyAtOuter"+colSuffix).c_str(), *privateData_->pyAtOuter, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"pzAtOuter"+colSuffix).c_str(), *privateData_->pzAtOuter, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"xAtOuter"+colSuffix).c_str(), *privateData_->xAtOuter, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"yAtOuter"+colSuffix).c_str(), *privateData_->yAtOuter, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"zAtOuter"+colSuffix).c_str(), *privateData_->zAtOuter, nCandString.c_str(), 0, "Reco");
+
+  if(saveFatTrk_) {
+    cmstree->column((colPrefix+"pxAtInner"+colSuffix).c_str(), *privateData_->pxAtInner, nCandString.c_str(), 0, "Reco");
+    cmstree->column((colPrefix+"pyAtInner"+colSuffix).c_str(), *privateData_->pyAtInner, nCandString.c_str(), 0, "Reco");
+    cmstree->column((colPrefix+"pzAtInner"+colSuffix).c_str(), *privateData_->pzAtInner, nCandString.c_str(), 0, "Reco");
+    cmstree->column((colPrefix+"xAtInner"+colSuffix).c_str(), *privateData_->xAtInner, nCandString.c_str(), 0, "Reco");
+    cmstree->column((colPrefix+"yAtInner"+colSuffix).c_str(), *privateData_->yAtInner, nCandString.c_str(), 0, "Reco");
+    cmstree->column((colPrefix+"zAtInner"+colSuffix).c_str(), *privateData_->zAtInner, nCandString.c_str(), 0, "Reco");
+  }
+}
+
+void CmsElectronFiller::writeEcalInfo(const Candidate *cand, const edm::Event& iEvent, const edm::EventSetup& iSetup, SuperClusterRef sclusRef) {
+  bool hasBarrel=true;
+  bool hasEndcap=true;
+  if(&sclusRef) {
+    // Cluster related variables
+    privateData_->ecal->push_back(sclusRef->energy());
+    privateData_->nClu->push_back(sclusRef->clustersSize());
+    
+    int ncry=0;
+    reco::basicCluster_iterator bcItr;
+    for(bcItr=sclusRef->clustersBegin(); bcItr!=sclusRef->clustersEnd(); bcItr++){
+      reco::BasicClusterRef bclusRef = *bcItr;
+      ncry+=bclusRef->getHitsByDetId().size();
+    }
+    privateData_->nCry->push_back(ncry);
+    
+    if(saveFatEcal_) {
+      privateData_->eraw->push_back(sclusRef->rawEnergy());
+      privateData_->caloEta->push_back(sclusRef->eta());
+      privateData_->caloPhi->push_back(sclusRef->phi());
+    }
+
+    // Cluster shape variables
+    Handle<BasicClusterShapeAssociationCollection> barrelClShpHandle;
+    try { iEvent.getByLabel("hybridSuperClusters","hybridShapeAssoc", barrelClShpHandle); }
+    catch ( cms::Exception& ex ) { LogWarning("CmsElectronFiller") << "Can't get ECAL barrel Cluster Shape Collection"; }
+    const reco::BasicClusterShapeAssociationCollection& barrelClShpMap = *barrelClShpHandle;
+
+    Handle<BasicClusterShapeAssociationCollection> endcapClShpHandle;
+    try { iEvent.getByLabel("islandBasicClusters","islandEndcapShapeAssoc", endcapClShpHandle); }
+    catch ( cms::Exception& ex ) { LogWarning("CmsElectronFiller") << "Can't get ECAL endcap Cluster Shape Collection"; }
+    const reco::BasicClusterShapeAssociationCollection& endcapClShpMap = *endcapClShpHandle;
+
+    reco::BasicClusterShapeAssociationCollection::const_iterator seedShpItr;
+    seedShpItr = barrelClShpMap.find(sclusRef->seed());
+    if(seedShpItr==barrelClShpMap.end()) {
+      LogDebug("CmsElectronFiller") << "This ECAL cluster has not barrel hit, looking for encap ones";
+      hasBarrel=false;
+      seedShpItr=endcapClShpMap.find(sclusRef->seed());
+      if(seedShpItr==endcapClShpMap.end()) hasEndcap=false;
+    }
+    if(hasBarrel || hasEndcap) {
+      const ClusterShapeRef& sClShape = seedShpItr->val;  
+
+      privateData_->e3x3->push_back(sClShape->e3x3());
+      privateData_->e5x5->push_back(sClShape->e5x5());
+      privateData_->eMax->push_back(sClShape->eMax());
+      privateData_->lat->push_back(sClShape->lat());
+      privateData_->phiLat->push_back(sClShape->phiLat());
+      privateData_->etaLat->push_back(sClShape->etaLat());
+      if(saveFatEcal_) {
+	privateData_->e2x2->push_back(sClShape->e2x2());
+	privateData_->e2nd->push_back(sClShape->e2nd());
+	privateData_->s1s9->push_back(sClShape->eMax()/sClShape->e3x3());
+	privateData_->s9s25->push_back(sClShape->e3x3()/sClShape->e5x5());
+	privateData_->covEtaEta->push_back(sClShape->covEtaEta());
+	privateData_->covEtaPhi->push_back(sClShape->covEtaPhi());
+	privateData_->covPhiPhi->push_back(sClShape->covPhiPhi());
+ 	privateData_->a20->push_back(sClShape->zernike20());
+ 	privateData_->a42->push_back(sClShape->zernike42());
+      }
+    }
+    else { LogWarning("CmsElectronFiller") << "Cannot find hits in ECAL barrel or ECAL encap. Why are you requesting filling ECAL infos?";}
+  }
+  if(!(&sclusRef) || ((&sclusRef) && (!hasBarrel & !hasEndcap)) ) {
+    privateData_->ecal->push_back(-1.);
+    privateData_->nClu->push_back(-1);
+    privateData_->nCry->push_back(-1);
+    privateData_->e3x3->push_back(-1.);
+    privateData_->e5x5->push_back(-1.);
+    privateData_->eMax->push_back(-1.);
+    privateData_->lat->push_back(-1.);
+    privateData_->phiLat->push_back(-1.);
+    privateData_->etaLat->push_back(-1.);
+    if(saveFatEcal_) {
+      privateData_->eraw->push_back(-1.);
+      privateData_->caloEta->push_back(-1.);
+      privateData_->caloPhi->push_back(-1.);
+      privateData_->e2x2->push_back(-1.);
+      privateData_->e2nd->push_back(-1.);
+      privateData_->s1s9->push_back(-1.);
+      privateData_->s9s25->push_back(-1.);
+      privateData_->covEtaEta->push_back(-1.);
+      privateData_->covEtaPhi->push_back(-1.);
+      privateData_->covPhiPhi->push_back(-1.);
+      privateData_->a20->push_back(-1.);
+      privateData_->a42->push_back(-1.);
+    }
+  }
+
+}
+
+void CmsElectronFiller::treeEcalInfo(const std::string &colPrefix, const std::string &colSuffix) {
+
+  std::string nCandString = colPrefix+(*trkIndexName_)+colSuffix;
+  cmstree->column((colPrefix+"ecal"+colSuffix).c_str(), *privateData_->ecal, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"nClu"+colSuffix).c_str(), *privateData_->nClu, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"nCry"+colSuffix).c_str(), *privateData_->nCry, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"e3x3"+colSuffix).c_str(), *privateData_->e3x3, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"e5x5"+colSuffix).c_str(), *privateData_->e5x5, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"eMax"+colSuffix).c_str(), *privateData_->eMax, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"lat"+colSuffix).c_str(), *privateData_->lat, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"phiLat"+colSuffix).c_str(), *privateData_->phiLat, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"etaLat"+colSuffix).c_str(), *privateData_->etaLat, nCandString.c_str(), 0, "Reco");
+  
+  if(saveFatEcal_) {
+    cmstree->column((colPrefix+"eraw"+colSuffix).c_str(), *privateData_->eraw, nCandString.c_str(), 0, "Reco");
+    cmstree->column((colPrefix+"caloEta"+colSuffix).c_str(), *privateData_->caloEta, nCandString.c_str(), 0, "Reco");
+    cmstree->column((colPrefix+"caloPhi"+colSuffix).c_str(), *privateData_->caloPhi, nCandString.c_str(), 0, "Reco");
+    cmstree->column((colPrefix+"e2x2"+colSuffix).c_str(), *privateData_->e2x2, nCandString.c_str(), 0, "Reco");
+    cmstree->column((colPrefix+"e2nd"+colSuffix).c_str(), *privateData_->e2nd, nCandString.c_str(), 0, "Reco");
+    cmstree->column((colPrefix+"s1s9"+colSuffix).c_str(), *privateData_->s1s9, nCandString.c_str(), 0, "Reco");
+    cmstree->column((colPrefix+"s9s25"+colSuffix).c_str(), *privateData_->s9s25, nCandString.c_str(), 0, "Reco");
+    cmstree->column((colPrefix+"covEtaEta"+colSuffix).c_str(), *privateData_->covEtaEta, nCandString.c_str(), 0, "Reco");
+    cmstree->column((colPrefix+"covEtaPhi"+colSuffix).c_str(), *privateData_->covEtaPhi, nCandString.c_str(), 0, "Reco");
+    cmstree->column((colPrefix+"covPhiPhi"+colSuffix).c_str(), *privateData_->covPhiPhi, nCandString.c_str(), 0, "Reco");
+    cmstree->column((colPrefix+"a20"+colSuffix).c_str(), *privateData_->a20, nCandString.c_str(), 0, "Reco");
+    cmstree->column((colPrefix+"a42"+colSuffix).c_str(), *privateData_->a42, nCandString.c_str(), 0, "Reco");
+  }
+}
+
+void CmsElectronFillerData::initialise() {
+
+  pxAtInner = new vector<float>;
+  pyAtInner = new vector<float>;
+  pzAtInner = new vector<float>;
+  xAtInner = new vector<float>;
+  yAtInner = new vector<float>;
+  zAtInner = new vector<float>;
+  pxAtOuter = new vector<float>;
+  pyAtOuter = new vector<float>;
+  pzAtOuter = new vector<float>;
+  xAtOuter = new vector<float>;
+  yAtOuter = new vector<float>;
+  zAtOuter = new vector<float>;
+
+  ecal = new vector<float>;
+  nClu = new vector<int>;
+  nCry = new vector<int>;
+  eraw = new vector<float>;
+  caloEta = new vector<float>;
+  caloPhi = new vector<float>;
+  eMax = new vector<float>;
+  e2nd = new vector<float>;
+  s1s9 = new vector<float>;
+  s9s25 = new vector<float>;
+  e2x2 = new vector<float>;
+  e3x3 = new vector<float>;
+  e5x5 = new vector<float>;
+  covEtaEta = new vector<float>;
+  covEtaPhi = new vector<float>;
+  covPhiPhi = new vector<float>;
+  lat = new vector<float>;
+  phiLat = new vector<float>;
+  etaLat = new vector<float>;
+  a20 = new vector<float>;
+  a42 = new vector<float>;
+
+}
+
+void CmsElectronFillerData::clearTrkVectors() {
+
+  pxAtOuter->clear();
+  pyAtOuter->clear();
+  pzAtOuter->clear();
+  xAtOuter->clear();
+  yAtOuter->clear();
+  zAtOuter->clear();
+  pxAtInner->clear();
+  pyAtInner->clear();
+  pzAtInner->clear();
+  xAtInner->clear();
+  yAtInner->clear();
+  zAtInner->clear();
+
+  ecal->clear();
+  nClu->clear();
+  nCry->clear();
+  e3x3->clear();
+  e5x5->clear();
+  eMax->clear();
+  lat->clear();
+  phiLat->clear();
+  etaLat->clear();
+  eraw->clear();
+  caloEta->clear();
+  caloPhi->clear();
+  e2x2->clear();
+  e2nd->clear();
+  s1s9->clear();
+  s9s25->clear();
+  covEtaEta->clear();
+  covEtaPhi->clear();
+  covPhiPhi->clear();
+  a20->clear();
+  a42->clear();
+
+}
