1c70351fdc4401a198b151d69b6a4a9265b44646
emanuele <>
2007-11-26 18:32:42 +0000

    added muon filler

---
+++ interface/CmsMuonFiller.h
@@ -0,0 +1,91 @@
+// -*- C++ -*-
+//-----------------------------------------------------------------------
+//
+// Package:    
+//      ZjetsAnalysis/ZllProducer
+// Description:
+//      Class CmsMuonFiller
+//      Simple class for dumping RECO (or AOD) contents to a ROOT tree
+//      
+//-----------------------------------------------------------------------
+
+#ifndef CmsMuonFiller_h
+#define CmsMuonFiller_h
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/MuonFwd.h"
+
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsTree.h"
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsCandidateFiller.h"
+#include "CLHEP/HepMC/GenEvent.h"
+#include <TTree.h>
+
+using namespace cms;
+using namespace edm;
+using namespace reco;
+
+struct CmsMuonFillerData : public CmsCandidateFillerData {
+
+public:
+  void initialise();
+  void clearTrkVectors();
+
+};
+
+class CmsMuonFiller : public CmsCandidateFiller {
+
+ public:
+
+  // Constructors
+
+  // Dump everything
+  CmsMuonFiller(CmsTree *, 
+		int maxTracks=500, int maxMCTracks=2000, 
+		bool noOutputIfLimitsReached=false );
+
+  // Dump  everything if fatTree is true and less informations otherwise
+  CmsMuonFiller(CmsTree *, 
+		bool fatTree, 
+		int maxTracks=500, int maxMCTracks=2000, 
+		bool noOutputIfLimitsReached=false );
+  
+  // Destructor
+  virtual ~CmsMuonFiller();
+
+  // Operators
+
+  // run number and all of that --- to implement
+
+  void writeCollectionToTree(const CandidateCollection *,
+			     const edm::Event&, const edm::EventSetup&,
+			     const std::string &columnPrefix, const std::string &columnSuffix,
+			     bool dumpData=false);
+
+ private:
+  
+  void writeMuonInfo(const Candidate *cand, const edm::Event&, const edm::EventSetup&);
+  void treeMuonInfo(const std::string &colPrefix, const std::string &colSuffix);
+
+  bool hitLimitsMeansNoOutput_;
+  int maxTracks_;
+  int maxMCTracks_;
+
+  std::string *trkIndexName_;
+
+  CmsMuonFillerData *privateData_;
+  edm::InputTag matchMap_;
+
+  CmsTree *cmstree;
+
+};
+
+#endif // CmsMuonFiller_h
---
+++ src/CmsMuonFiller.cc
@@ -0,0 +1,177 @@
+//---------------------------------------------------------------------------
+//
+// Description:
+//       Package:   HiggsAnalysis/HiggsToWW2e
+//       Class:     CmsMuonFiller
+//
+//-----------------------------------------------------------------------
+
+// system include files
+#include <memory>
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "DataFormats/RecoCandidate/interface/RecoCandidate.h"
+#include "DataFormats/RecoCandidate/interface/RecoEcalCandidate.h"
+
+#include "DataFormats/Candidate/interface/CandMatchMap.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticleCandidate.h"
+#include "PhysicsTools/HepMCCandAlgos/interface/MCCandMatcher.h"
+
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/MuonFwd.h"
+
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsTree.h"
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsCandidateFiller.h"
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsMuonFiller.h"
+
+#include "CLHEP/HepMC/GenParticle.h"
+
+
+#include <TTree.h>
+
+#include <string>
+
+using namespace edm;
+using namespace reco;
+
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+//----------------
+// Constructors --
+//----------------
+
+
+CmsMuonFiller::CmsMuonFiller(CmsTree *cmsTree, 
+			     int maxTracks, int maxMCTracks,
+			     bool noOutputIfLimitsReached):
+  CmsCandidateFiller(cmsTree,maxTracks,maxMCTracks,noOutputIfLimitsReached),
+  privateData_(new CmsMuonFillerData)
+{
+  cmstree=cmsTree;
+
+  trkIndexName_ = new std::string("n");
+
+  hitLimitsMeansNoOutput_ = noOutputIfLimitsReached;
+  maxTracks_=maxTracks;
+  maxMCTracks_=maxMCTracks;
+
+  privateData_->initialise();
+}
+
+CmsMuonFiller::CmsMuonFiller(CmsTree *cmsTree, 
+			     bool fatTree, 
+			     int maxTracks, int maxMCTracks,
+			     bool noOutputIfLimitsReached):
+  CmsCandidateFiller(cmsTree,fatTree,maxTracks,maxMCTracks,noOutputIfLimitsReached),
+  privateData_(new CmsMuonFillerData)
+{
+  cmstree=cmsTree;
+
+  trkIndexName_ = new std::string("n");
+
+  hitLimitsMeansNoOutput_ = noOutputIfLimitsReached;
+  maxTracks_=maxTracks;
+  maxMCTracks_=maxMCTracks;
+
+  privateData_->initialise();
+}
+
+
+//--------------
+// Destructor --
+//--------------
+
+CmsMuonFiller::~CmsMuonFiller() { }
+
+
+//-------------
+// Methods   --
+//-------------
+
+void CmsMuonFiller::writeCollectionToTree(const CandidateCollection *collection,
+					 const edm::Event& iEvent, const edm::EventSetup& iSetup,
+					 const std::string &columnPrefix, const std::string &columnSuffix,
+					 bool dumpData) {
+
+  privateData_->clearTrkVectors();
+
+  if(collection) {
+    if(hitLimitsMeansNoOutput_ && 
+       (int)collection->size() > maxTracks_){
+      LogInfo("CmsMuonFiller") << "Track length " << collection->size() 
+			       << " is too long for declared max length for tree "
+			       << maxTracks_ << " and no output flag is set."
+			       << " No tracks written to tuple for this event ";
+      return;
+    }
+
+    if((int)collection->size() > maxTracks_){
+      LogInfo("CmsMuonFiller") << "Track length " << collection->size() 
+			       << " is too long for declared max length for tree "
+			       << maxTracks_ 
+			       << ". Collection will be truncated ";
+    }
+  
+    *(privateData_->ncand) = collection->size();
+
+    CandidateCollection::const_iterator cand;
+    for(cand=collection->begin(); cand!=collection->end(); cand++) {
+      // fill basic kinematics
+      if(saveCand_) writeCandInfo(&(*cand),iEvent,iSetup);
+    }
+  }
+  else {
+  *(privateData_->ncand) = 0;
+  }
+  
+  // The class member vectors containing the relevant quantities 
+  // have all been filled. Now transfer those we want into the 
+  // tree 
+  int blockSize = (collection) ? collection->size() : 0;
+  std::string nCandString = columnPrefix+(*trkIndexName_)+columnSuffix; 
+  cmstree->column(nCandString.c_str(),blockSize,0,"Reco");
+  
+  if(saveCand_) treeCandInfo(columnPrefix,columnSuffix);
+
+  if(dumpData) cmstree->dumpData();
+	
+}
+
+
+
+
+
+
+
+
+
+void CmsMuonFiller::treeMuonInfo(const std::string &colPrefix, const std::string &colSuffix) {
+
+  std::string nCandString = colPrefix+(*trkIndexName_)+colSuffix;
+}
+
+
+
+
+
+
+
+void CmsMuonFillerData::initialise() {
+
+  initialiseCandidate();
+}
+
+void CmsMuonFillerData::clearTrkVectors() {
+
+  clearTrkVectorsCandidate();
+}
