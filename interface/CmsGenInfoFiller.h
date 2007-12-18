c90a20967dbe6b3ee2bc9369196dba2620d9d978
emanuele <>
2007-12-18 16:31:05 +0000

    class to dump generator info to run on soups

---
+++ interface/CmsGenInfoFiller.h
@@ -0,0 +1,59 @@
+// -*- C++ -*-
+//-----------------------------------------------------------------------
+//
+// Package:    
+//      HtoWWTreeDumper
+// Description:
+//      Class CmsGenInfoFiller
+//      Simple class for dumping RECO (or AOD) contents to an ntuple
+//      
+// Original Author:  Alessio Ghezzi, Pietro Govoni
+//         Created:  Fri Apr  6 18:05:34 CEST 2007
+//
+//-----------------------------------------------------------------------
+
+#ifndef CmsGenInfoFiller_h
+#define CmsGenInfoFiller_h
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/Common/interface/Handle.h"
+#include "SimDataFormats/HepMCProduct/interface/GenInfoProduct.h"
+#include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
+
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsTree.h"
+
+
+#include "CLHEP/HepMC/GenEvent.h"
+#include <TTree.h>
+
+class CmsGenInfoFillerData;
+
+class CmsGenInfoFiller {
+
+ public:
+
+  //! Constructors
+  CmsGenInfoFiller(CmsTree *);
+
+  //! Destructor
+  virtual ~CmsGenInfoFiller();
+
+  // to be used also in AODSIM 
+  void
+  writeGenInfoToTree (double processID, double ptHat, double genFilterEff,  double genXsec,  double weight);
+
+  void 
+  writeGenInfoToTree (edm::Handle<GenInfoProduct> & gi, edm::Handle<HepMCProduct>& mc, double weight );
+
+ private:
+  
+  CmsGenInfoFillerData *privateData_;
+
+};
+
+#endif // CmsGenInfoFiller_h
---
+++ src/CmsGenInfoFiller.cc
@@ -0,0 +1,116 @@
+//---------------------------------------------------------------------------
+//
+// Description:
+//       Package:   HtoWWTreeDumper
+//       Class:     CmsGenInfoFiller
+//
+// Original Author:  Emanuele Di Marco
+//         Created:  Mon Apr  9 11:01:00 CEST 2007
+//-----------------------------------------------------------------------
+
+// system include files
+#include <memory>
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "DataFormats/METReco/interface/CaloMET.h"
+#include "DataFormats/METReco/interface/CaloMETCollection.h"
+#include "DataFormats/METReco/interface/GenMET.h"
+#include "DataFormats/METReco/interface/GenMETCollection.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/EgammaReco/interface/BasicCluster.h"
+#include "DataFormats/EgammaReco/interface/SuperCluster.h"
+#include "DataFormats/EgammaReco/interface/BasicClusterShapeAssociation.h"
+#include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectron.h"
+#include "DataFormats/EgammaCandidates/interface/PixelMatchElectron.h"
+#include "DataFormats/EgammaCandidates/interface/PixelMatchElectronFwd.h"
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/Candidate/interface/CandMatchMap.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticleCandidate.h"
+
+#include "DataFormats/RecoCandidate/interface/RecoCandidate.h"
+#include "DataFormats/RecoCandidate/interface/RecoEcalCandidate.h"
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsTree.h"
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsEleIDTreeFiller.h"
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsGenInfoFiller.h"
+
+#include "CLHEP/HepMC/GenEvent.h"
+
+#include "PhysicsTools/HepMCCandAlgos/interface/MCCandMatcher.h"
+#include "CLHEP/HepMC/GenParticle.h"
+
+#include "FWCore/Framework/interface/TriggerNames.h"
+
+#include <TTree.h>
+#include <string>
+
+using namespace edm;
+using namespace reco
+;
+struct CmsGenInfoFillerData {
+  CmsTree *cmstree;
+  double processID;
+  double ptHat;
+  double genFilterEff;
+  double genCrossXsec;
+  double weight;
+};
+
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+//----------------
+// Constructors --
+//----------------
+
+
+CmsGenInfoFiller::CmsGenInfoFiller(CmsTree *cmstree):
+  privateData_(new CmsGenInfoFillerData)
+{
+  privateData_->cmstree=cmstree;
+
+}
+
+
+// ---------------------------------------------------------------
+
+
+CmsGenInfoFiller::~CmsGenInfoFiller() {
+}
+
+
+// ---------------------------------------------------------------
+void
+CmsGenInfoFiller::writeGenInfoToTree (double processID, double ptHat, double genFilterEff,  double genXsec,  double weight)
+{
+  
+  std::cout << processID << " " << ptHat << " " << genFilterEff << " " << genXsec << " " << weight << std::endl;
+  privateData_->cmstree->column ("genProcessId", processID, 0., "Gen");
+  privateData_->cmstree->column ("genPtHat", ptHat, 0., "Gen");
+  privateData_->cmstree->column ("genFilterEff", genFilterEff, 0., "Gen");
+  privateData_->cmstree->column ("genXsec", genXsec, 0., "Gen");
+  privateData_->cmstree->column ("genWeight", weight, 0., "Gen");
+  return;
+}
+
+// ---------------------------------------------------------------
+void 
+CmsGenInfoFiller::writeGenInfoToTree (edm::Handle<edm::GenInfoProduct> & gi, edm::Handle<edm::HepMCProduct>& mc, double weight )
+{
+  const HepMC::GenEvent *genEvt = mc->GetEvent();
+    
+  double processID = genEvt->signal_process_id();
+  double pthat = genEvt->event_scale(); 
+
+  double external_cross_section = gi->external_cross_section(); // is the precalculated one written in the cfg file -- units is pb
+  double filter_eff = gi->filter_efficiency();
+
+  writeGenInfoToTree(processID,pthat,filter_eff, external_cross_section, weight);
+}
