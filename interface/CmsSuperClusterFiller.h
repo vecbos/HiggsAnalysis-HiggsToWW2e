1fb0011c0a71bdd75545a3926c9ed404192df646
emanuele <>
2007-12-18 16:31:18 +0000

    class to dump super clusters

---
+++ interface/CmsSuperClusterFiller.h
@@ -0,0 +1,90 @@
+// -*- C++ -*-
+//-----------------------------------------------------------------------
+//
+// Package:    
+//      HiggsAnalysis/HiggsToWW2e
+// Description:
+//      Class CmsSuperClusterFiller
+//      Simple class for dumping RECO (or AOD) contents to a ROOT tree
+//      
+// Original Author:  Emanuele Di Marco
+//         Created:  Fri Apr  6 18:05:34 CEST 2007
+//
+//-----------------------------------------------------------------------
+
+#ifndef CmsSuperClusterFiller_h
+#define CmsSuperClusterFiller_h
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h"
+#include "DataFormats/EgammaReco/interface/SuperCluster.h"
+
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsTree.h"
+#include <TTree.h>
+
+using namespace cms;
+using namespace edm;
+using namespace reco;
+
+struct CmsSuperClusterFillerData {
+  // All the vectors that will store the stuff
+  // going into the tree.
+  vector<int>  *nBC, *nCrystals, *iAlgo;
+  vector<float> *rawEnergy, *energy, *eta, *phi;
+  int *nSC;
+
+public:
+  void initialiseCandidate();
+  void clear();
+};
+
+
+class CmsSuperClusterFiller {
+
+ public:
+
+  // Constructors
+
+  // Dump everything
+  CmsSuperClusterFiller(CmsTree *, int maxSC=500);
+
+
+  // Destructor
+  virtual ~CmsSuperClusterFiller();
+
+  // Operators
+
+  // run number and all of that --- to implement
+
+  virtual void writeCollectionToTree(const SuperClusterCollection *,
+			     const edm::Event&, const edm::EventSetup&,
+			     const std::string &columnPrefix, const std::string &columnSuffix,
+			     bool dumpData=false);
+
+
+ protected:
+  
+
+  virtual void writeSCInfo(const SuperCluster *cand, 
+		     const edm::Event&, const edm::EventSetup&);
+  virtual void treeSCInfo(const std::string colPrefix, const std::string colSuffix);
+  
+  // Friends
+
+  CmsSuperClusterFillerData *privateData_;
+
+  CmsTree *cmstree;
+
+  int maxSC_;
+  std::string *trkIndexName_;
+};
+
+#endif // CmsSuperClusterFiller_h
---
+++ src/CmsSuperClusterFiller.cc
@@ -0,0 +1,202 @@
+//---------------------------------------------------------------------------
+//
+// Description:
+//       Package:   HtoWWTreeDumper
+//       Class:     CmsSuperClusterFiller
+//
+// Original Author:  Emanuele Di Marco
+//         Created:  Mon Apr  9 11:01:00 CEST 2007
+//-----------------------------------------------------------------------
+
+// system include files
+#include <memory>
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "DataFormats/METReco/interface/CaloMET.h"
+#include "DataFormats/METReco/interface/CaloMETCollection.h"
+#include "DataFormats/METReco/interface/GenMET.h"
+#include "DataFormats/METReco/interface/GenMETCollection.h"
+
+#include "DataFormats/JetReco/interface/CaloJet.h"
+#include "DataFormats/JetReco/interface/CaloJetCollection.h"
+#include "DataFormats/JetReco/interface/GenJet.h"
+
+#include "DataFormats/RecoCandidate/interface/RecoCandidate.h"
+#include "DataFormats/RecoCandidate/interface/RecoEcalCandidate.h"
+
+#include "DataFormats/TrackReco/interface/Track.h"
+
+#include "DataFormats/EgammaReco/interface/BasicCluster.h"
+#include "DataFormats/EgammaReco/interface/SuperCluster.h"
+#include "DataFormats/DetId/interface/DetId.h"
+
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsTree.h"
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsSuperClusterFiller.h"
+
+#include <TTree.h>
+
+#include <string>
+
+using namespace edm;
+using namespace reco;
+
+
+
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+//----------------
+// Constructors --
+//----------------
+
+
+CmsSuperClusterFiller::CmsSuperClusterFiller(CmsTree *cmsTree, int maxSC):  privateData_(new CmsSuperClusterFillerData)
+{
+  cmstree=cmsTree;
+
+  trkIndexName_ = new std::string("n");
+  maxSC_=maxSC;
+  privateData_->initialiseCandidate();
+}
+
+//--------------
+// Destructor --
+//--------------
+
+CmsSuperClusterFiller::~CmsSuperClusterFiller() 
+{
+  // delete here the vector ptr's
+  delete privateData_->nBC;
+  delete privateData_->nCrystals;
+  delete privateData_->iAlgo;
+  delete privateData_->rawEnergy;
+  delete privateData_->energy;
+  delete privateData_->eta;
+  delete privateData_->phi;
+}
+
+
+//-------------
+// Methods   --
+//-------------
+
+// Set boolean control options for quantities that are written out
+
+
+void CmsSuperClusterFiller::writeCollectionToTree(const SuperClusterCollection *collection,
+						  const edm::Event& iEvent, const edm::EventSetup& iSetup,
+						  const std::string &columnPrefix, const std::string &columnSuffix,
+						  bool dumpData) 
+{
+  privateData_->clear();
+  
+  if(collection) 
+    {
+      if((int)collection->size() > maxSC_)
+	{
+	  LogError("CmsSuperClusterFiller") << "Track length " << collection->size() 
+					    << " is too long for declared max length for tree "
+					    << maxSC_ 
+					    << ". Collection will be truncated ";
+	}
+      
+      *(privateData_->nSC) = collection->size();
+      
+      SuperClusterCollection::const_iterator cand;
+      for(cand=collection->begin(); cand!=collection->end(); cand++) 
+	{
+	  // fill basic kinematics
+	  writeSCInfo(&(*cand),iEvent,iSetup);
+	}
+    }
+  else 
+    {
+      *(privateData_->nSC) = 0;
+    }
+  
+  // The class member vectors containing the relevant quantities 
+  // have all been filled. Now transfer those we want into the 
+  // tree 
+  
+  int blockSize = (collection) ? collection->size() : 0;
+  
+  std::string nCandString = columnPrefix+(*trkIndexName_)+columnSuffix; 
+  cmstree->column(nCandString.c_str(),blockSize,0,"Reco");
+  
+//   if(collection) 
+//     {
+      treeSCInfo(columnPrefix,columnSuffix);
+//     }
+
+  if(dumpData) cmstree->dumpData();
+
+}
+
+
+
+
+
+
+void CmsSuperClusterFiller::writeSCInfo(const SuperCluster *cand, 
+				       const edm::Event& iEvent, 
+				       const edm::EventSetup& iSetup) 
+{
+  privateData_->nBC->push_back((int)cand->clustersSize());
+  privateData_->nCrystals->push_back((int)cand->getHitsByDetId().size());
+  privateData_->iAlgo->push_back((int)cand->seed()->algo());
+  privateData_->rawEnergy->push_back((float)cand->rawEnergy());
+  privateData_->energy->push_back((float)cand->energy());
+  privateData_->eta->push_back((float)cand->position().eta());
+  privateData_->phi->push_back((float)cand->position().phi());
+}
+
+
+
+
+
+
+
+void CmsSuperClusterFiller::treeSCInfo(const std::string colPrefix, const std::string colSuffix) 
+{
+  std::string nCandString = colPrefix+(*trkIndexName_)+colSuffix;
+  cmstree->column((colPrefix+"SCnBC"+colSuffix).c_str(), *privateData_->nBC, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"SCnCrystals"+colSuffix).c_str(), *privateData_->nCrystals, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"SCiAlgo"+colSuffix).c_str(), *privateData_->iAlgo, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"SCrawEnergy"+colSuffix).c_str(), *privateData_->rawEnergy, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"SCenergy"+colSuffix).c_str(), *privateData_->energy, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"SCeta"+colSuffix).c_str(), *privateData_->eta, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"SCphi"+colSuffix).c_str(), *privateData_->phi, nCandString.c_str(), 0, "Reco");
+}
+
+
+
+void CmsSuperClusterFillerData::initialiseCandidate() 
+{
+  nBC = new vector<int>;
+  nCrystals = new vector<int>; 
+  iAlgo = new vector<int>;
+  rawEnergy = new vector<float>; 
+  energy = new vector<float>; 
+  eta = new vector<float>; 
+  phi = new vector<float>;;
+  nSC =  new int;
+}
+
+void CmsSuperClusterFillerData::clear() 
+{
+  nBC->clear();
+  nCrystals->clear();
+  iAlgo->clear();
+  rawEnergy->clear();
+  energy->clear();
+  eta->clear();
+  phi->clear();
+}
