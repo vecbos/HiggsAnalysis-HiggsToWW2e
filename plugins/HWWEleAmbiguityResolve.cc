4c9eb623f61f30c651c79fc25b85f328eeada782
emanuele <>
2008-03-05 23:17:25 +0000

    convert to filtering by reference

--- plugins/HWWEleAmbiguityResolve.cc
+++ plugins/HWWEleAmbiguityResolve.cc
@@ -22,22 +22,20 @@ HWWEleAmbiguityResolve::HWWEleAmbiguityResolve (const edm::ParameterSet& conf)
 
 
 void 
-HWWEleAmbiguityResolve::select (edm::Handle<collection> inputHandle, 
-                        const edm::Event& evt, const edm::EventSetup& evtStp ) 
+HWWEleAmbiguityResolve::select (edm::Handle<collection> input, const edm::Event& evt ) 
 {
-  m_selected.clear () ;
-  //PG get the actual product
 
-  dump (&m_selected, inputHandle) ;
+  m_selected.clear() ;
+  ambEle.clear();
 
-  std::vector<const reco::PixelMatchGsfElectron *>::iterator ShorterEnd;
+  edm::LogInfo("HWWEleAmbiguityResolve") << "HWWEleAmbiguityResolve starting, original collection size = " 
+					 << input->size();
+  Init(input);
+  
+  ResolveByEoverP(input);
 
-  sort (m_selected.begin (), m_selected.end (), TKeEoPSorting ()) ; 
-  ShorterEnd = unique (m_selected.begin (), m_selected.end (), equalTk ()) ; 
-  m_selected.erase(ShorterEnd, m_selected.end());
-  sort (m_selected.begin (), m_selected.end (), SCEeEoPSorting ()) ; 
-  ShorterEnd = unique (m_selected.begin (), m_selected.end (), equalSC ()) ; 
-  m_selected.erase (ShorterEnd, m_selected.end());
+  edm::LogInfo("HWWEleAmbiguityResolve") << "WWEleAmbiguityResolve ending, final collection size = " 
+					 << m_selected.size();
 
   return ;
 }       
@@ -50,17 +48,91 @@ HWWEleAmbiguityResolve::~HWWEleAmbiguityResolve()
 {}
 
 
-// ------------------------------------------------------------
+void HWWEleAmbiguityResolve::Init(const edm::Handle<collection> & input) {
 
+  // This method is doing the check for ambiguity, where ambiguity is
+  // 2 electron candidates sharing the same cluster or the same track.
+  // This method fills : 
+  // --- resolvedEle collections with the no-ambigus electrons
+  
+  int no_ambiguity;
+  std::vector<unsigned int> amb_index;
 
-void 
-HWWEleAmbiguityResolve::dump (container * output, 
-                              const edm::Handle<collection> & input) 
-{
-  for (collection::const_iterator it = input->begin () ;
-       it != input->end () ;
-       ++it)
-    { output->push_back (&(*it)) ; }   
-  return ;
+  for (unsigned int ii=0; ii < input->size(); ii++){  
+
+    no_ambiguity=0;
+ 
+    // first electron
+    const reco::PixelMatchGsfElectron & thisEle1 = (*input)[ii];
+    reco::GsfTrackRef thisTrack1  = (thisEle1).gsfTrack();
+    reco::SuperClusterRef thisSc1 = (thisEle1).superCluster();
+
+    // 2nd loop over all the electron candidates
+    for (unsigned int jj=ii+1; jj<input->size(); jj++){
+
+      // second electron
+      const reco::PixelMatchGsfElectron & thisEle2 = (*input)[jj];
+      reco::GsfTrackRef thisTrack2  = (thisEle2).gsfTrack();
+      reco::SuperClusterRef thisSc2 = (thisEle2).superCluster();
+
+      // sharing sc or track
+      if ( (thisSc1 == thisSc2) || (thisTrack1 == thisTrack2) ){      
+        amb_index.push_back(jj);
+        ambEle.push_back(std::pair<unsigned int, unsigned int>(ii, jj));
+      }
+      else { no_ambiguity++; }
+    }
+    
+    bool test=true;
+    for (unsigned int iii=0; iii<amb_index.size(); iii++){
+      if (amb_index[iii] == ii ) test=false;
+    }
+    if ((unsigned int)no_ambiguity == (unsigned int)input->size()-1-ii && test==true) {
+      reco::PixelMatchGsfElectronRef electronRef(input, ii);
+      edm::LogInfo("HWWEleAmbiguityResolve") << "easy, not ambiguous at all. SuperClusterRef->energy() = " 
+					     << electronRef->superCluster()->energy();
+      m_selected.push_back(electronRef);
+    }
+  }
 }
+
+
+void HWWEleAmbiguityResolve::ResolveByEoverP(const edm::Handle<collection> & input)
+{
+  // This method resolves the ambiguities using E(supercluster) over P , 
+  // the closest to one is declared as the good candidate.
+  // It uses the map associating the ambigus electron candidates
+  // It continues filling:
+  // --- resolvedEle collections with the chosen electrons candidates 
+
+  if ( input->size() !=0 ) {
+
+    std::vector<std::pair<unsigned int, unsigned int> >::iterator it;
+
+    for ( it=ambEle.begin(); it<ambEle.end(); it++){
+      
+      // loop over electrons which have one or more than one ambiguity
+      int bestEleId=it->first;
+      int multiAmbEleId=(int)it->first;
+      while((int)it->first==multiAmbEleId && it<ambEle.end()) {
+	//	std::cout << "multiAmbEleId = " << multiAmbEleId << "\tbestEleId = " << bestEleId << std::endl;
+	const reco::PixelMatchGsfElectron & bestEle = (*input)[bestEleId];
+	const reco::PixelMatchGsfElectron & compEle = (*input)[it->second];
+	edm::LogInfo("HWWEleAmbiguityResolve") << "compEle.superCluster()->energy() = " 
+					       << compEle.superCluster()->energy() 
+					       << "\tbestEle.superCluster()->energy() = " 
+					       << bestEle.superCluster()->energy();
+	if(fabs(compEle.eSuperClusterOverP()-1) <= fabs(bestEle.eSuperClusterOverP()-1)) bestEleId=it->second;
+	it++;
+      }
+      edm::LogInfo("HWWEleAmbiguityResolve") << "looping on the ambiguous electrons: best ele index = " 
+					     <<  bestEleId << std::endl;
+      reco::PixelMatchGsfElectronRef electronRef(input, bestEleId);
+      m_selected.push_back(electronRef);
+    }
+    
+  }
+
+}
+
 
--- plugins/HWWEleAmbiguityResolve.h
+++ plugins/HWWEleAmbiguityResolve.h
@@ -20,7 +20,7 @@ class HWWEleAmbiguityResolve{
 
   typedef const reco::PixelMatchGsfElectron * electron ;
   typedef reco::PixelMatchGsfElectronCollection collection ;
-  typedef std::vector<const reco::PixelMatchGsfElectron *> container ;
+  typedef reco::PixelMatchGsfElectronRefVector container;
   typedef container::const_iterator const_iterator ;
   
   //! ctor
@@ -33,72 +33,25 @@ class HWWEleAmbiguityResolve{
   
   //! iterator to the end of the selected collection
   const_iterator end () const { return m_selected.end () ; }
+
   //! the actual selector method 
-  void select (edm::Handle<collection>, 
-               const edm::Event&, const edm::EventSetup& ) ;
-  
-  //! to sort the GSF electrons std::vector according to the E/p ratio and E
-  //! if the tracks are different, the sorting is performed by the supercluster raw energy
-  //! if the tracks are the same, the sorting is performed by using the |E/P - 1| function
-  struct SCEeEoPSorting : public std::binary_function<electron, electron, bool> 
-    {
-      bool operator() (electron ele1, electron ele2) 
-        { 
-          if ( &(*(ele1->superCluster())) == &(*(ele2->superCluster()))  )
-          return fabs(ele1->eSuperClusterOverP ()-1) < 
-                   fabs(ele2->eSuperClusterOverP ()-1) ;
-          return (ele1->superCluster ())->rawEnergy () < 
-                   (ele2->superCluster ())->rawEnergy () ;
-        }
-    };
-        
-
-  //! to sort the GSF electrons std::vector according to the E/p ratio and TK
-  //! if the tracks are different, the sorting is performed by the p() of the tk
-  //! if the tracks are the same, the sorting is performed by using the |E/P - 1| function
-  struct TKeEoPSorting : public std::binary_function<electron, electron, bool> 
-    {
-      bool operator() (electron ele1, electron ele2) 
-        { 
-          if (ele1->gsfTrack () == ele2->gsfTrack ())         
-          return fabs(ele1->eSuperClusterOverP ()-1) < 
-                   fabs(ele2->eSuperClusterOverP ()-1) ;
-          return (ele1->gsfTrack ())->p () < (ele2->gsfTrack ())->p () ;
-        }
-      };
-      
-
-  //! check if two GSF electrons refer to the same track
-  struct equalTk : public std::binary_function<electron, electron, bool> 
-    {
-      bool operator() (electron ele1, electron ele2) 
-        { return ele1->gsfTrack () == ele2->gsfTrack () ; }
-    };
-
-
-  //! check if two GSF electrons contain the same supercluster
-  struct equalSC : public std::binary_function<electron, electron, bool> 
-    {      bool operator() (electron ele1, electron ele2) 
-        { return ( &(*(ele1->superCluster())) == &(*(ele2->superCluster()))  ); }
-     };
+  void select (edm::Handle<collection>, const edm::Event& ) ;
      
 
  private:
+
+  //! find the non ambiguous electrons, initialise the ambiguity map
+  void Init(const edm::Handle<collection> & input);
+  
+  //! ambiguity resolution
+  void ResolveByEoverP(const edm::Handle<collection> & input);
+
   //! the selected collection
   container m_selected ;
-  //! fill the ouptut with the pointers to the elements of the input
-  void dump (container * output, const edm::Handle<collection> & input) ;
  
+  //! map between the ambiguous electrons
+  std::vector<std::pair<unsigned int, unsigned int> > ambEle;
 };  
 
 #endif
 
-/* FIXME
-
-nn mi e' chiara la funzione (o l'uso) del m_selected, perche'
-c'e' il container di puntatori? come funziona il selector dell'object?
-e' solo un trucco per non perdere tempo a copiare?
-NON riesco a capire esattamente che cosa dovrebbe fare! 
-se poi ritorna iteratori, a chi puntano questi?
-
-*/
