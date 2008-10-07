e1addc42cff137e5f4b7fb4696d365b8fd4dc8f0
emanuele <>
2008-10-07 11:35:08 +0000

    run the PF met algorithm in the met producer sequence (PF met not in the event)

--- python/metProducerSequence_cff.py
+++ python/metProducerSequence_cff.py
@@ -4,4 +4,9 @@ from RecoMET.Configuration.GenMETParticles_cff import *
 from RecoMET.Configuration.RecoGenMET_cff import *
 from JetMETCorrections.Type1MET.MetType1Corrections_cff import *
 
-metSequence = cms.Sequence ( genCandidatesForMET + genMet + corMetType1Icone5 )
+pfMET = cms.EDProducer("PFMET",
+                       PFCandidates = cms.InputTag("particleFlow"),
+                       verbose = cms.untracked.bool(False)
+                       )
+
+metSequence = cms.Sequence ( pfMET )
