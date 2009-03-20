13c3074308aeba12a5d442a71623fe2c4646ad89
emanuele <>
2009-03-20 17:55:09 +0000

    added jet correction sequences

--- python/jetProducerSequence_cff.py
+++ python/jetProducerSequence_cff.py
@@ -1,4 +1,5 @@
 import FWCore.ParameterSet.Config as cms
+from JetMETCorrections.Configuration.L2L3Corrections_Summer08_cff import *
 
 jetVertexAlpha1 = cms.EDProducer("JetVertexAssociation",
                                  JV_deltaZ = cms.double(0.3),
@@ -6,11 +7,10 @@ jetVertexAlpha1 = cms.EDProducer("JetVertexAssociation",
                                  JV_alpha_threshold = cms.double(0.2),
                                  JV_cone_size = cms.double(0.5),
                                  JV_type_Algo = cms.int32(1),
-                                 JET_ALGO = cms.string('iterativeCone5CaloJets'),
+                                 JET_ALGO = cms.string('L2L3CorJetSC5Calo'),
                                  TRACK_ALGO = cms.string('generalTracks'),
                                  VERTEX_ALGO = cms.string('offlinePrimaryVertices'),
-                                 JV_cutType = cms.string('delta')
-                                 )
+                                 JV_cutType = cms.string('delta') )
 
 jetVertexAlpha2 = cms.EDProducer("JetVertexAssociation",
                                  JV_deltaZ = cms.double(0.3),
@@ -21,7 +21,6 @@ jetVertexAlpha2 = cms.EDProducer("JetVertexAssociation",
                                  JET_ALGO = cms.string('sisCone5CaloJets'),
                                  TRACK_ALGO = cms.string('generalTracks'),
                                  VERTEX_ALGO = cms.string('offlinePrimaryVertices'),
-                                 JV_cutType = cms.string('delta')
-                                 )
+                                 JV_cutType = cms.string('delta') )
 
-jetSequence = cms.Sequence( jetVertexAlpha1 + jetVertexAlpha2 )
+jetSequence = cms.Sequence( L2L3CorJetSC5Calo + L2L3CorJetSC5PF + jetVertexAlpha1 + jetVertexAlpha2 )
