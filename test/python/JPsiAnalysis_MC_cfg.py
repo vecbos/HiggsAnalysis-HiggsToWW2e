fd5fb3eff116707e84a9f603b9b5b0a90e1613a0
emanuele <>
2009-11-27 14:59:11 +0000

    first commit

---
+++ test/python/JPsiAnalysis_MC_cfg.py
@@ -0,0 +1,64 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("JPsiAnalysis")
+
+process.extend(cms.include("RecoEcal/EgammaClusterProducers/data/geometryForClustering.cff"))
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = 'MC_31X_V8::All'
+
+# --- apply the muon correction from the common code
+process.load("HiggsAnalysis.HiggsToWW2Leptons.HWWMetCorrector_cfi")
+
+# --- jet met sequences ---
+process.load("HiggsAnalysis.HiggsToWW2e.jetProducerSequence_cff")
+process.load("HiggsAnalysis.HiggsToWW2e.metProducerSequence_cff")
+
+# --- electron sequences ---
+process.load("RecoEgamma.EgammaIsolationAlgos.eleIsolationSequence_cff")
+process.load("HiggsAnalysis.HiggsToWW2e.ambiguityResolvedElectrons_cfi")
+process.load("HiggsAnalysis.HiggsToWW2e.electronIdSequence_cff")
+
+# --- track sequences ---
+process.load("HiggsAnalysis.HiggsToWW2e.trackCandidates_cfi")
+
+# --- tree dumper ---
+process.load("HiggsAnalysis.HiggsToWW2e.treeDumper_cfi")
+process.treeDumper.nameFile = 'default.root'
+process.treeDumper.dumpTriggerResults = True
+process.treeDumper.dumpGenInfo = True
+process.treeDumper.dumpSignalKfactor = False
+process.treeDumper.dumpSCs = True
+process.treeDumper.dumpTracks = True
+process.treeDumper.dumpVertices = True
+process.treeDumper.dumpParticleFlowObjects = True
+process.treeDumper.saveFatTrk = True
+process.treeDumper.saveJet1BTag = True
+process.treeDumper.saveJet2BTag = False
+process.treeDumper.dumpTree = True
+
+process.options = cms.untracked.PSet(
+      fileMode =  cms.untracked.string('NOMERGE')
+      )
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+                            noEventSort = cms.untracked.bool(True),
+                            duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
+                            debugFlag = cms.untracked.bool(True),
+                            debugVebosity = cms.untracked.uint32(10),
+#                           fileNames = cms.untracked.vstring('file:/cmsrm/pc18/crovelli/JPsiEE_reco_200eve.root')
+                            fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/user/e/emanuele/RECO/JpsiEE_31X.root')
+                            )
+
+process.p = cms.Path ( process.muonCorrectedMET *
+                       process.jetSequence * process.pfjetSCSequence * process.newBtaggingSequence *
+                       process.eIdSequence *
+                       process.eleIsolationSequence *
+                       process.ambiguityResolvedElectrons *
+                       process.trackCandidates)
+                       
+process.q = cms.EndPath ( process.treeDumper )
