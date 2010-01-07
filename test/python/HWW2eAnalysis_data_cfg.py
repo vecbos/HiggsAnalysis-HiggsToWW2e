7e489c291b7b62a7be81a4fb6c73133da0b72ef7
emanuele <>
2010-01-07 16:56:36 +0000

    first commit

---
+++ test/python/HWW2eAnalysis_data_cfg.py
@@ -0,0 +1,73 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("HiggsToWW2e")
+
+process.extend(cms.include("RecoEcal/EgammaClusterProducers/data/geometryForClustering.cff"))
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = 'MC_31X_V8::All'
+
+# --- common code to comput gg fusion signal k factor
+process.load("HiggsAnalysis.HiggsToWW2Leptons.HWWKFactorProducer_cfi")
+
+# --- apply the muon correction from the common code
+process.load("HiggsAnalysis.HiggsToWW2Leptons.HWWMetCorrector_cfi")
+
+# --- jet met sequences ---
+process.load("HiggsAnalysis.HiggsToWW2e.jetProducerSequence_cff")
+process.load("HiggsAnalysis.HiggsToWW2e.metProducerSequence_cff")
+
+# --- electron sequences ---
+process.load("RecoEgamma.EgammaIsolationAlgos.eleIsolationSequence_cff")
+process.load("HiggsAnalysis.HiggsToWW2e.ambiguityResolvedElectrons_cfi")
+process.load("HiggsAnalysis.HiggsToWW2e.electronIdSequence_cff")
+
+# --- track sequences ---
+process.load("RecoTracker.DeDx.dedxEstimatorsFromRefitter_cff")
+
+# --- ECAL clusters merging in a unique collection ---
+process.load("HiggsAnalysis.HiggsToWW2e.superClusterMerger_cfi")
+
+# --- tree dumper ---
+process.load("HiggsAnalysis.HiggsToWW2e.treeDumper_cfi")
+process.treeDumper.nameFile = 'default.root'
+process.treeDumper.dumpTriggerResults = True
+process.treeDumper.dumpGenInfo = False
+process.treeDumper.dumpMCTruth = False
+process.treeDumper.dumpSignalKfactor = False
+process.treeDumper.dumpGenMet = False
+process.treeDumper.dumpGenJets = False
+process.treeDumper.dumpTracks = True
+process.treeDumper.dumpGsfTracks = True
+process.treeDumper.dumpSCs = True
+process.treeDumper.dumpVertices = True
+process.treeDumper.dumpParticleFlowObjects = True
+process.treeDumper.saveFatTrk = True
+process.treeDumper.saveJet1BTag = True
+process.treeDumper.saveJet2BTag = False
+process.treeDumper.dumpTree = True
+
+process.options = cms.untracked.PSet(
+      fileMode =  cms.untracked.string('NOMERGE')
+      )
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+                            noEventSort = cms.untracked.bool(True),
+                            duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
+                            debugFlag = cms.untracked.bool(True),
+                            debugVebosity = cms.untracked.uint32(10),
+                            fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/cms/store/caf/user/meridian/MinimumBias/BeamCommissioning09_BSCFilter_v6/c32fbdf401a82bf088a335dc690defa6/bscFilter_124120_8.root')
+                            )
+
+process.p = cms.Path ( process.muonCorrectedMET *
+                       process.mergedSuperClusters *
+                       process.jetSequence * process.pfjetSCSequence * process.newBtaggingSequence *
+                       process.eIdSequence *
+                       process.eleIsolationSequence *
+                       process.ambiguityResolvedElectrons)
+                       
+process.q = cms.EndPath ( process.treeDumper )
