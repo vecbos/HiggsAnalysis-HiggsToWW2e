7b6972dba8d1901e9d2a702aac5b1680ef2ec965
emanuele <>
2010-02-17 16:35:46 +0000

    change name to akT jets (both calojets and PF jets) to match the ones of the Summer09 production

--- python/jetProducerSequence_cff.py
+++ python/jetProducerSequence_cff.py
@@ -1,8 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 from JetMETCorrections.Configuration.L2L3Corrections_Summer09_7TeV_cff import *
-# temporary fix for antiKT name mismatch
-L2L3CorJetAK5Calo.src = "antikt5CaloJets"
-L2L3CorJetAK5PF.src = "antikt5PFJets"
 
 from HiggsAnalysis.HiggsToWW2e.btagProducerSequence_cff import *
 
@@ -23,7 +20,7 @@ jetVertexAlpha2 = cms.EDProducer("JetVertexAssociation",
                                  JV_alpha_threshold = cms.double(0.2),
                                  JV_cone_size = cms.double(0.5),
                                  JV_type_Algo = cms.int32(1),
-                                 JET_ALGO = cms.string('antikt5CaloJets'),
+                                 JET_ALGO = cms.string('ak5CaloJets'),
                                  TRACK_ALGO = cms.string('generalTracks'),
                                  VERTEX_ALGO = cms.string('offlinePrimaryVertices'),
                                  JV_cutType = cms.string('delta') )
