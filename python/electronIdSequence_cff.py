aa3dd09a2978e201cc5f6ee13327bb8f65958bc5
emanuele <>
2010-04-22 19:55:23 +0000

    run the elecron ID with cuts sequence proper of the 3_6_0 instead of the one of 3_5_6

--- python/electronIdSequence_cff.py
+++ python/electronIdSequence_cff.py
@@ -1,8 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoEgamma.ElectronIdentification.electronIdLikelihoodExt_cfi import *
+from RecoEgamma.ElectronIdentification.electronIdSequence_cff import *
 
+from RecoEgamma.ElectronIdentification.electronIdLikelihoodExt_cfi import *
 import RecoEgamma.ElectronIdentification.electronIdLikelihoodExt_cfi
 egammaIDLikelihood = RecoEgamma.ElectronIdentification.electronIdLikelihoodExt_cfi.eidLikelihoodExt.clone()
 
-eIdSequence = cms.Sequence( egammaIDLikelihood )
+eIdSequence = cms.Sequence( eIdSequence * egammaIDLikelihood )
