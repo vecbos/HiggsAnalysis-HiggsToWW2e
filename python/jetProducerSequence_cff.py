7de56408e135db7622dd47ce4ca286f69c34c440
emanuele <>
2010-11-10 12:13:53 +0000

    take the corrections from GlobalTag

--- python/jetProducerSequence_cff.py
+++ python/jetProducerSequence_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 from JetMETCorrections.Configuration.DefaultJEC_cff import *
-from JetMETCorrections.Configuration.JetCorrectionCondDB_cff import *
+#from JetMETCorrections.Configuration.JetCorrectionCondDB_cff import *
 
 # ESSources for JPT corrections from file (until JPT corrections are not in DB)
 from HiggsAnalysis.HiggsToWW2e.jptL2L3Corrections_cff import *
