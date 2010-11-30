f69edadf74fedba09876a156262dd32893d89ecb
emanuele <>
2010-11-30 15:52:21 +0000

    update the GT for the new JES corrections

--- test/python/VecBosAnalysis_data_cfg.py
+++ test/python/VecBosAnalysis_data_cfg.py
@@ -6,7 +6,7 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.Reconstruction_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'GR10_P_V11::All'
+process.GlobalTag.globaltag = 'GR_R_38X_V15::All'
 
 # --- jet met sequences ---
 process.load("HiggsAnalysis.HiggsToWW2e.jetProducerSequence_cff")
