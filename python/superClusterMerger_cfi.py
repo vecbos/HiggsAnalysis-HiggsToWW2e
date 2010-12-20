a17c5fa8855dcbc1d372080c830123e021ceec64
emanuele <>
2010-12-20 16:00:30 +0000

    another fix for EE collection: use the corrected one with preshower

--- python/superClusterMerger_cfi.py
+++ python/superClusterMerger_cfi.py
@@ -2,5 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 mergedSuperClusters = cms.EDProducer("SuperClusterCombiner",
                                      labels = cms.VInputTag(cms.InputTag("correctedHybridSuperClusters"),
-                                                            cms.InputTag("multi5x5SuperClustersWithPreshower"))
+                                                            cms.InputTag("correctedMulti5x5SuperClustersWithPreshower"))
                                      )
