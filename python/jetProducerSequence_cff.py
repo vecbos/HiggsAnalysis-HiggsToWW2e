99f891f68cab96d6f3f04acb5eda36be1bcf43d9
emanuele <>
2010-12-22 17:17:26 +0000

    differentiate MC / Data (for the residual corrections)

--- python/jetProducerSequence_cff.py
+++ python/jetProducerSequence_cff.py
@@ -1,3 +1,5 @@
+#jetCorrectionsData = 1
+
 import FWCore.ParameterSet.Config as cms
 
 from JetMETCorrections.Configuration.DefaultJEC_cff import *
@@ -26,11 +28,19 @@ PUcorrAk5PFJetsL1.src = 'PUcorrAk5PFJets'
 PUcorrAk5PFJetsL1.correctors = ['PUcorrL1Fastjet']
 
 PUcorrAk5PFJetsL1L2L3Residual   = ak5PFJetsL2L3.clone(src = 'PUcorrAk5PFJetsL1', correctors = ['ak5PFL2L3Residual'])
-offsetCorrection = cms.Sequence(PUcorrAk5PFJets * PUcorrKt6PFJets * PUcorrAk5PFJetsL1 * PUcorrAk5PFJetsL1L2L3Residual)
+PUcorrAk5PFJetsL1L2L3  = ak5PFJetsL2L3.clone(src = 'PUcorrAk5PFJetsL1', correctors = ['ak5PFL2L3'])
 
-CaloJetSequence = cms.Sequence( ak5CaloJetsL2L3Residual )
-PFJetAK5Sequence = cms.Sequence( ak5PFJetsL2L3Residual * offsetCorrection * PUcorrAk5PFJetsL1L2L3Residual )
-JPTjetsAK5Sequence = cms.Sequence( ak5JPTJetsL2L3Residual ) # not run for the moment
+offsetCorrection = cms.Sequence(PUcorrAk5PFJets * PUcorrKt6PFJets * PUcorrAk5PFJetsL1)
 
-ourJetSequence = cms.Sequence( CaloJetSequence * PFJetAK5Sequence )
+# data sequences use residual corrections
+CaloJetSequenceData = cms.Sequence( ak5CaloJetsL2L3Residual )
+PFJetAK5SequenceData = cms.Sequence( ak5PFJetsL2L3Residual * offsetCorrection * PUcorrAk5PFJetsL1L2L3Residual )
+JPTjetsAK5SequenceData = cms.Sequence( ak5JPTJetsL2L3Residual ) # not run for the moment
+ourJetSequenceData = cms.Sequence( CaloJetSequenceData * PFJetAK5SequenceData )
+
+# MC sequeces use only L2L3 corrections
+CaloJetSequenceMC = cms.Sequence( ak5CaloJetsL2L3 )
+PFJetAK5SequenceMC = cms.Sequence( ak5PFJetsL2L3 * offsetCorrection * PUcorrAk5PFJetsL1L2L3 )
+JPTjetsAK5SequenceMC = cms.Sequence( ak5JPTJetsL2L3 ) # not run for the moment
+ourJetSequenceMC = cms.Sequence( CaloJetSequenceMC * PFJetAK5SequenceMC )
 
