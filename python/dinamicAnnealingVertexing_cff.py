10e79f57b450ac047903f5defee56e6847e45de8
emanuele <>
2011-03-05 16:39:10 +0000

    new result

---
+++ python/dinamicAnnealingVertexing_cff.py
@@ -0,0 +1,12 @@
+# re-reconstructing the primary vertices with the Deterministic Annealing (DA) vertex finder
+# from B. Mangano studies
+import FWCore.ParameterSet.Config as cms
+
+from RecoVertex.PrimaryVertexProducer.OfflinePrimaryVerticesDA_cfi import *
+import RecoVertex.PrimaryVertexProducer.OfflinePrimaryVerticesDA_cfi
+
+offlinePrimaryVertices = RecoVertex.PrimaryVertexProducer.OfflinePrimaryVerticesDA_cfi.offlinePrimaryVerticesDA.clone()
+offlinePrimaryVertices.useBeamConstraint = cms.bool(True)
+offlinePrimaryVertices.TkClusParameters.TkDAClusParameters.Tmin = cms.double(4.)
+offlinePrimaryVertices.TkClusParameters.TkDAClusParameters.vertexSize = cms.double(0.01)
+
