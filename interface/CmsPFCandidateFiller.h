d69e24dd1d55bc7bf9a32b59a52543cbe84ff64c
emanuele <>
2011-05-28 17:36:52 +0000

    first commit

---
+++ interface/CmsPFCandidateFiller.h
@@ -0,0 +1,88 @@
+// -*- C++ -*-
+//-----------------------------------------------------------------------
+//
+// Package:    
+//      HiggsAnalysis/HiggsToWW2e
+// Description:
+//      Class CmsMetFiller
+//      Simple class for dumping RECO (or AOD) contents to a ROOT tree
+//      
+// Original Author:  Emanuele Di Marco
+//         Created:  Fri Apr  6 18:05:34 CEST 2007
+//
+//-----------------------------------------------------------------------
+
+#ifndef CmsPFCandidateFiller_h
+#define CmsPFCandidateFiller_h
+
+#include "FWCore/Framework/interface/Event.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/Candidate/interface/CompositeCandidate.h"
+#include "DataFormats/Candidate/interface/VertexCompositeCandidate.h"
+#include "DataFormats/RecoCandidate/interface/RecoChargedCandidate.h"
+#include "DataFormats/RecoCandidate/interface/RecoChargedCandidateFwd.h"
+
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsTree.h"
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsCandidateFiller.h"
+#include <TTree.h>
+
+struct CmsPFCandidateFillerData : public CmsCandidateFillerData {
+  // All the vectors that will store the stuff
+  // going into the tree.
+  vector<int> *particleId, *vtxtype;
+  vector<int> *superClusterRef, *trackRef, *gsfTrackRef;
+
+public:
+  void initialise();
+  void clearTrkVectors();
+};
+
+
+class CmsPFCandidateFiller : public CmsCandidateFiller {
+
+ public:
+
+  //! Dump everything
+  CmsPFCandidateFiller(CmsTree *, int maxTracks=5000, 
+		    int maxMCTracks=5000, bool noOutputIfLimitsReached=false );
+
+  // Destructor
+  virtual ~CmsPFCandidateFiller();
+    
+  //! set the general tracks (reduced collection)
+  void setGeneralTracks( edm::InputTag generalTracks) { generalTracks_ = generalTracks; }
+
+  //! write the basic candidate informations for the collection "collection"
+  virtual void writeCollectionToTree(edm::InputTag collection,
+				     const edm::Event&, const edm::EventSetup&,
+				     const std::string &columnPrefix, const std::string &columnSuffix,
+				     bool dumpData=false);
+  
+
+ protected:
+  
+
+  virtual void treePFInfo(const std::string colPrefix, const std::string colSuffix);
+  
+
+  // Friends
+
+  CmsPFCandidateFillerData *privateData_;
+
+  CmsTree *cmstree;
+
+  bool hitLimitsMeansNoOutput_;
+  int maxTracks_;
+  int maxMCTracks_;
+
+  edm::InputTag generalTracks_;
+  edm::Handle< reco::TrackRefVector > h_tracks;
+
+  std::string *trkIndexName_;
+};
+
+#endif // CmsPFCandidateFiller_h
---
+++ src/CmsPFCandidateFiller.cc
@@ -0,0 +1,194 @@
+//---------------------------------------------------------------------------
+//
+// Description:
+//       Package:   HtoWWTreeDumper
+//       Class:     CmsPFCandidateFiller
+//
+// Original Author:  Emanuele Di Marco
+//         Created:  Mon Apr  9 11:01:00 CEST 2007
+//-----------------------------------------------------------------------
+
+// system include files
+#include <memory>
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "HiggsAnalysis/HiggsToWW2e/interface/CmsPFCandidateFiller.h"
+
+#include <TTree.h>
+
+#include <string>
+
+using namespace edm;
+using namespace reco;
+
+
+
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+//----------------
+// Constructors --
+//----------------
+
+
+CmsPFCandidateFiller::CmsPFCandidateFiller(CmsTree *cmsTree, int maxTracks, 
+                                           int maxMCTracks,
+                                           bool noOutputIfLimitsReached):
+  CmsCandidateFiller(cmsTree,maxTracks,maxMCTracks,noOutputIfLimitsReached),
+  privateData_(new CmsPFCandidateFillerData)
+{
+  cmstree=cmsTree;
+
+  trkIndexName_ = new std::string("n");
+
+  hitLimitsMeansNoOutput_ = noOutputIfLimitsReached;
+  maxTracks_=maxTracks;
+  maxMCTracks_=maxMCTracks;
+
+  privateData_->initialise();
+}
+
+
+//--------------
+// Destructor --
+//--------------
+
+CmsPFCandidateFiller::~CmsPFCandidateFiller() {
+
+  // delete here the vector ptr's
+  delete privateData_->particleId;
+  delete privateData_->trackRef;
+  delete privateData_->gsfTrackRef;
+  delete privateData_->superClusterRef;
+
+}
+
+
+//-------------
+// Methods   --
+//-------------
+
+
+
+
+void CmsPFCandidateFiller::writeCollectionToTree(edm::InputTag collectionTag,
+					       const edm::Event& iEvent, const edm::EventSetup& iSetup,
+					       const std::string &columnPrefix, const std::string &columnSuffix,
+					       bool dumpData) {
+
+  edm::Handle< edm::View<reco::Candidate> > collectionHandle;
+  try { iEvent.getByLabel(collectionTag, collectionHandle); }
+  catch ( cms::Exception& ex ) { edm::LogWarning("CmsPFCandidateFiller") << "Can't get candidate collection: " << collectionTag; }
+  const edm::View<reco::Candidate> *collection = collectionHandle.product();
+
+  privateData_->clearTrkVectors();
+
+  std::string nCandString = columnPrefix+(*trkIndexName_)+columnSuffix; 
+  LogInfo("CmsPFCandidateFiller") << "=== Writing collection " << nCandString << " ===";
+
+  if(collection) {
+    if(hitLimitsMeansNoOutput_ && 
+       (int)collection->size() > maxTracks_){
+      LogError("CmsPFCandidateFiller") << "Track length " << collection->size() 
+				     << " is too long for declared max length for tree "
+				     << maxTracks_ << " and no output flag is set."
+				     << " No tracks written to tuple for this event ";
+      return;
+    }
+
+    if((int)collection->size() > maxTracks_){
+      LogError("CmsPFCandidateFiller") << "Track length " << collection->size() 
+				     << " is too long for declared max length for tree "
+				     << maxTracks_ 
+				     << ". Collection will be truncated ";
+    }
+  
+    *(privateData_->ncand) = collection->size();
+
+    // for track link
+    try { iEvent.getByLabel(generalTracks_, h_tracks); }
+    catch ( cms::Exception& ex ) { edm::LogWarning("CmsElectronFiller") << "Can't get general track collection: " << generalTracks_; }
+
+    edm::View<reco::Candidate>::const_iterator cand;
+    for(cand=collection->begin(); cand!=collection->end(); cand++) {
+      // fill basic kinematics
+      if(saveCand_) writeCandInfo(&(*cand),iEvent,iSetup);
+      const reco::PFCandidate *pfcand = dynamic_cast< const reco::PFCandidate * > ( &(*cand) );
+      privateData_->particleId->push_back(pfcand->particleId());
+
+      int ctfTrackLink=-1;
+      for(int itrk=0; itrk<(int)h_tracks->size(); itrk++) {
+        reco::TrackRef iTrackRef = h_tracks->at(itrk);
+        if(pfcand->trackRef().key() == iTrackRef.key()) {
+          ctfTrackLink = itrk;
+          break;
+        }
+      }
+
+      privateData_->trackRef->push_back(ctfTrackLink);
+      privateData_->gsfTrackRef->push_back(pfcand->gsfTrackRef().key());
+      privateData_->superClusterRef->push_back(pfcand->superClusterRef().key());
+    }
+  }
+  else {
+    *(privateData_->ncand) = 0;
+  }
+
+  // The class member vectors containing the relevant quantities 
+  // have all been filled. Now transfer those we want into the 
+  // tree 
+
+  int blockSize = (collection) ? collection->size() : 0;
+  
+  cmstree->column(nCandString.c_str(),blockSize,0,"Reco");
+
+  if(saveCand_) treeCandInfo(columnPrefix,columnSuffix);
+  treePFInfo(columnPrefix,columnSuffix);
+
+  if(dumpData) cmstree->dumpData();
+
+}
+
+
+
+
+
+
+
+void CmsPFCandidateFiller::treePFInfo(const std::string colPrefix, const std::string colSuffix) {
+
+  std::string nCandString = colPrefix+(*trkIndexName_)+colSuffix;
+  cmstree->column((colPrefix+"particleId"+colSuffix).c_str(), *privateData_->particleId, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"trackRef"+colSuffix).c_str(), *privateData_->trackRef, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"gsfTrackRef"+colSuffix).c_str(), *privateData_->gsfTrackRef, nCandString.c_str(), 0, "Reco");
+  cmstree->column((colPrefix+"superClusterRef"+colSuffix).c_str(), *privateData_->superClusterRef, nCandString.c_str(), 0, "Reco");
+}
+
+
+
+
+
+
+
+void CmsPFCandidateFillerData::initialise() {
+
+  initialiseCandidate();
+  particleId = new vector<int>;
+  trackRef = new vector<int>;
+  gsfTrackRef = new vector<int>;
+  superClusterRef = new vector<int>;
+
+}
+
+void CmsPFCandidateFillerData::clearTrkVectors() {
+
+  clearTrkVectorsCandidate();
+  particleId->clear();
+  trackRef->clear();
+  gsfTrackRef->clear();
+  superClusterRef->clear();
+
+}
