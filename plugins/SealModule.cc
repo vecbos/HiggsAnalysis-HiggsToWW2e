50da98609cff56fcb36d3b5f5178765b72901a77
emanuele <>
2011-05-28 14:38:56 +0000

    add the lepton linked track selector

--- plugins/SealModule.cc
+++ plugins/SealModule.cc
@@ -7,6 +7,7 @@
 #include "CommonTools/UtilAlgos/interface/ObjectSelector.h"
 #include "RecoMuon/TrackerSeedGenerator/plugins/CollectionCombiner.h"
 #include "HiggsAnalysis/HiggsToWW2e/plugins/HWWEleAmbiguityResolve.h"
+#include "HiggsAnalysis/HiggsToWW2e/plugins/LeptonTrackFilter.h"
 #include "HiggsAnalysis/HiggsToWW2e/plugins/HWWTreeDumper.h"
 
 
@@ -14,10 +15,15 @@ typedef ObjectSelector< HWWEleAmbiguityResolve,
 			reco::GsfElectronRefVector
 			> AmbResolver ;
 
+typedef ObjectSelector< LeptonTrackFilter, 
+			reco::TrackRefVector
+			> leptonTrackFilter ;
+
 typedef CollectionCombiner< reco::SuperClusterCollection > SuperClusterCombiner;
 typedef CollectionCombiner< reco::BasicClusterCollection > BasicClusterCombiner;
 
 DEFINE_FWK_MODULE (AmbResolver) ;
+DEFINE_FWK_MODULE (leptonTrackFilter);
 DEFINE_FWK_MODULE(SuperClusterCombiner);
 DEFINE_FWK_MODULE(BasicClusterCombiner);
 DEFINE_FWK_MODULE (HWWTreeDumper);  
