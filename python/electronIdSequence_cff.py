23bada8f78c96037efc2199f49f306db52dc8c87
emanuele <>
2011-05-28 14:41:05 +0000

    do not use useless modules

--- python/electronIdSequence_cff.py
+++ python/electronIdSequence_cff.py
@@ -1,19 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-# to run likelihood and standard CIC
-from RecoEgamma.ElectronIdentification.cutsInCategoriesElectronIdentificationV06_DataTuning_cfi import *
-eIdSequence = cms.Sequence(eidVeryLoose+eidLoose+eidMedium+eidTight+eidSuperTight+eidHyperTight1)
-
-# to run the HWW V04 CIC
-from HiggsAnalysis.HiggsToWW2e.cutsInCategoriesHWWElectronIdentificationV04_cfi import *
-eIdHWWSequence = cms.Sequence(eidHWWVeryLoose+eidHWWLoose+eidHWWMedium+eidHWWTight+eidHWWSuperTight+eidHWWHyperTight1+eidHWWHyperTight2+eidHWWHyperTight3)
-
 from RecoEgamma.ElectronIdentification.electronIdLikelihoodExt_cfi import *
 import RecoEgamma.ElectronIdentification.electronIdLikelihoodExt_cfi
 egammaIDLikelihood = RecoEgamma.ElectronIdentification.electronIdLikelihoodExt_cfi.eidLikelihoodExt.clone()
 
-eIdSequence = cms.Sequence( eIdSequence * eIdHWWSequence * egammaIDLikelihood )
-
+eIdSequence = cms.Sequence( egammaIDLikelihood )
 
 # to compute FastJet rho to correct isolation (note: EtaMax restricted to 2.5)
 from RecoJets.JetProducers.kt4PFJets_cfi import *
