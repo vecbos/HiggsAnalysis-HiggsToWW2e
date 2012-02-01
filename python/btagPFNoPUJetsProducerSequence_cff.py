ab5147337895eeedb56a321aefcae5f074289c7f
pandolf <>
2012-02-01 13:59:11 +0000

    corrected names, really

--- python/btagPFNoPUJetsProducerSequence_cff.py
+++ python/btagPFNoPUJetsProducerSequence_cff.py
@@ -6,100 +6,100 @@ from RecoBTag.Configuration.RecoBTag_cff import *
 
 # create a new jets and tracks association
 import RecoJets.JetAssociationProducers.ak5JTA_cff
-newPFJetNoPUTracksAssociatorAtVertex = RecoJets.JetAssociationProducers.ak5JTA_cff.ak5JetTracksAssociatorAtVertex.clone()
-newPFJetNoPUTracksAssociatorAtVertex.jets = "ak5PFJetsNoPUL1FastL2L3Residual"
-newPFJetNoPUTracksAssociatorAtVertex.tracks = "generalTracks"
+newPFNoPUJetTracksAssociatorAtVertex = RecoJets.JetAssociationProducers.ak5JTA_cff.ak5JetTracksAssociatorAtVertex.clone()
+newPFNoPUJetTracksAssociatorAtVertex.jets = "ak5PFNoPUJetsL1FastL2L3Residual"
+newPFNoPUJetTracksAssociatorAtVertex.tracks = "generalTracks"
 
 # impact parameter b-tag
 import RecoBTag.Configuration.RecoBTag_cff
-newPFJetsNoPUImpactParameterTagInfos = RecoBTag.Configuration.RecoBTag_cff.impactParameterTagInfos.clone()
-newPFJetsNoPUImpactParameterTagInfos.jetTracks = "newPFJetNoPUTracksAssociatorAtVertex"
-newTrackCountingHighEffBPFJetNoPUTags = RecoBTag.Configuration.RecoBTag_cff.trackCountingHighEffBJetTags.clone()
-newTrackCountingHighEffBPFJetNoPUTags.tagInfos = cms.VInputTag( cms.InputTag("newPFJetsNoPUImpactParameterTagInfos") )
-newTrackCountingHighPurBPFJetNoPUTags = RecoBTag.Configuration.RecoBTag_cff.trackCountingHighPurBJetTags.clone()
-newTrackCountingHighPurBPFJetNoPUTags.tagInfos = cms.VInputTag( cms.InputTag("newPFJetsNoPUImpactParameterTagInfos") )
-newJetProbabilityBPFJetNoPUTags = RecoBTag.Configuration.RecoBTag_cff.jetProbabilityBJetTags.clone()
-newJetProbabilityBPFJetNoPUTags.tagInfos = cms.VInputTag( cms.InputTag("newPFJetsNoPUImpactParameterTagInfos") )
-newJetBProbabilityBPFJetNoPUTags = RecoBTag.Configuration.RecoBTag_cff.jetBProbabilityBJetTags.clone()
-newJetBProbabilityBPFJetNoPUTags.tagInfos = cms.VInputTag( cms.InputTag("newPFJetsNoPUImpactParameterTagInfos") )
-newImpactParameterMVABPFJetNoPUTags = RecoBTag.Configuration.RecoBTag_cff.impactParameterMVABJetTags.clone()
-newImpactParameterMVABPFJetNoPUTags.tagInfos = cms.VInputTag( cms.InputTag("newPFJetsNoPUImpactParameterTagInfos") )
+newPFNoPUJetsImpactParameterTagInfos = RecoBTag.Configuration.RecoBTag_cff.impactParameterTagInfos.clone()
+newPFNoPUJetsImpactParameterTagInfos.jetTracks = "newPFNoPUJetTracksAssociatorAtVertex"
+newTrackCountingHighEffBPFNoPUJetTags = RecoBTag.Configuration.RecoBTag_cff.trackCountingHighEffBJetTags.clone()
+newTrackCountingHighEffBPFNoPUJetTags.tagInfos = cms.VInputTag( cms.InputTag("newPFNoPUJetsImpactParameterTagInfos") )
+newTrackCountingHighPurBPFNoPUJetTags = RecoBTag.Configuration.RecoBTag_cff.trackCountingHighPurBJetTags.clone()
+newTrackCountingHighPurBPFNoPUJetTags.tagInfos = cms.VInputTag( cms.InputTag("newPFNoPUJetsImpactParameterTagInfos") )
+newJetProbabilityBPFNoPUJetTags = RecoBTag.Configuration.RecoBTag_cff.jetProbabilityBJetTags.clone()
+newJetProbabilityBPFNoPUJetTags.tagInfos = cms.VInputTag( cms.InputTag("newPFNoPUJetsImpactParameterTagInfos") )
+newJetBProbabilityBPFNoPUJetTags = RecoBTag.Configuration.RecoBTag_cff.jetBProbabilityBJetTags.clone()
+newJetBProbabilityBPFNoPUJetTags.tagInfos = cms.VInputTag( cms.InputTag("newPFNoPUJetsImpactParameterTagInfos") )
+newImpactParameterMVABPFNoPUJetTags = RecoBTag.Configuration.RecoBTag_cff.impactParameterMVABJetTags.clone()
+newImpactParameterMVABPFNoPUJetTags.tagInfos = cms.VInputTag( cms.InputTag("newPFNoPUJetsImpactParameterTagInfos") )
 
 # secondary vertex b-tag
-newPFJetsNoPUSecondaryVertexTagInfos = RecoBTag.Configuration.RecoBTag_cff.secondaryVertexTagInfos.clone()
-newPFJetsNoPUSecondaryVertexTagInfos.trackIPTagInfos = "newPFJetsNoPUImpactParameterTagInfos"
-newSimpleSecondaryVertexHighEffBPFJetNoPUTags = RecoBTag.Configuration.RecoBTag_cff.simpleSecondaryVertexHighEffBJetTags.clone()
-newSimpleSecondaryVertexHighEffBPFJetNoPUTags.tagInfos = cms.VInputTag( cms.InputTag("newPFJetsNoPUSecondaryVertexTagInfos") )
-newSimpleSecondaryVertexHighPurBPFJetNoPUTags = RecoBTag.Configuration.RecoBTag_cff.simpleSecondaryVertexHighPurBJetTags.clone()
-newSimpleSecondaryVertexHighPurBPFJetNoPUTags.tagInfos = cms.VInputTag( cms.InputTag("newPFJetsNoPUSecondaryVertexTagInfos") )
-newCombinedSecondaryVertexBPFJetNoPUTags = RecoBTag.Configuration.RecoBTag_cff.combinedSecondaryVertexBJetTags.clone()
-newCombinedSecondaryVertexBPFJetNoPUTags.tagInfos = cms.VInputTag( cms.InputTag("newPFJetsNoPUImpactParameterTagInfos"), cms.InputTag("newPFJetsNoPUSecondaryVertexTagInfos") )
-newCombinedSecondaryVertexMVABPFJetNoPUTags = RecoBTag.Configuration.RecoBTag_cff.combinedSecondaryVertexMVABJetTags.clone()
-newCombinedSecondaryVertexMVABPFJetNoPUTags.tagInfos = cms.VInputTag( cms.InputTag("newPFJetsNoPUImpactParameterTagInfos"), cms.InputTag("newPFJetsNoPUSecondaryVertexTagInfos") )
+newPFNoPUJetsSecondaryVertexTagInfos = RecoBTag.Configuration.RecoBTag_cff.secondaryVertexTagInfos.clone()
+newPFNoPUJetsSecondaryVertexTagInfos.trackIPTagInfos = "newPFNoPUJetsImpactParameterTagInfos"
+newSimpleSecondaryVertexHighEffBPFNoPUJetTags = RecoBTag.Configuration.RecoBTag_cff.simpleSecondaryVertexHighEffBJetTags.clone()
+newSimpleSecondaryVertexHighEffBPFNoPUJetTags.tagInfos = cms.VInputTag( cms.InputTag("newPFNoPUJetsSecondaryVertexTagInfos") )
+newSimpleSecondaryVertexHighPurBPFNoPUJetTags = RecoBTag.Configuration.RecoBTag_cff.simpleSecondaryVertexHighPurBJetTags.clone()
+newSimpleSecondaryVertexHighPurBPFNoPUJetTags.tagInfos = cms.VInputTag( cms.InputTag("newPFNoPUJetsSecondaryVertexTagInfos") )
+newCombinedSecondaryVertexBPFNoPUJetTags = RecoBTag.Configuration.RecoBTag_cff.combinedSecondaryVertexBJetTags.clone()
+newCombinedSecondaryVertexBPFNoPUJetTags.tagInfos = cms.VInputTag( cms.InputTag("newPFNoPUJetsImpactParameterTagInfos"), cms.InputTag("newPFNoPUJetsSecondaryVertexTagInfos") )
+newCombinedSecondaryVertexMVABPFNoPUJetTags = RecoBTag.Configuration.RecoBTag_cff.combinedSecondaryVertexMVABJetTags.clone()
+newCombinedSecondaryVertexMVABPFNoPUJetTags.tagInfos = cms.VInputTag( cms.InputTag("newPFNoPUJetsImpactParameterTagInfos"), cms.InputTag("newPFNoPUJetsSecondaryVertexTagInfos") )
 
 # soft electron b-tag
-newPFJetsNoPUSoftElectronTagInfos = RecoBTag.Configuration.RecoBTag_cff.softElectronTagInfos.clone()
-newPFJetsNoPUSoftElectronTagInfos.jets = "ak5PFJetsL2L3Residual"
-newSoftElectronBPFJetNoPUTags = RecoBTag.Configuration.RecoBTag_cff.softElectronBJetTags.clone()
-newSoftElectronBPFJetNoPUTags.tagInfos = cms.VInputTag( cms.InputTag("newPFJetsNoPUSoftElectronTagInfos") )
-newSoftElectronByIP3dBPFJetNoPUTags = RecoBTag.Configuration.RecoBTag_cff.softElectronByIP3dBJetTags.clone()
-newSoftElectronByIP3dBPFJetNoPUTags.tagInfos = cms.VInputTag( cms.InputTag("newPFJetsNoPUSoftElectronTagInfos") )
-newSoftElectronByPtBPFJetNoPUTags = RecoBTag.Configuration.RecoBTag_cff.softElectronByPtBJetTags.clone()
-newSoftElectronByPtBPFJetNoPUTags.tagInfos = cms.VInputTag( cms.InputTag("newPFJetsNoPUSoftElectronTagInfos") )
+newPFNoPUJetsSoftElectronTagInfos = RecoBTag.Configuration.RecoBTag_cff.softElectronTagInfos.clone()
+newPFNoPUJetsSoftElectronTagInfos.jets = "ak5PFJetsL2L3Residual"
+newSoftElectronBPFNoPUJetTags = RecoBTag.Configuration.RecoBTag_cff.softElectronBJetTags.clone()
+newSoftElectronBPFNoPUJetTags.tagInfos = cms.VInputTag( cms.InputTag("newPFNoPUJetsSoftElectronTagInfos") )
+newSoftElectronByIP3dBPFNoPUJetTags = RecoBTag.Configuration.RecoBTag_cff.softElectronByIP3dBJetTags.clone()
+newSoftElectronByIP3dBPFNoPUJetTags.tagInfos = cms.VInputTag( cms.InputTag("newPFNoPUJetsSoftElectronTagInfos") )
+newSoftElectronByPtBPFNoPUJetTags = RecoBTag.Configuration.RecoBTag_cff.softElectronByPtBJetTags.clone()
+newSoftElectronByPtBPFNoPUJetTags.tagInfos = cms.VInputTag( cms.InputTag("newPFNoPUJetsSoftElectronTagInfos") )
 
 # soft muon b-tag
-newPFJetsNoPUSoftMuonTagInfos = RecoBTag.Configuration.RecoBTag_cff.softMuonTagInfos.clone()
-newPFJetsNoPUSoftMuonTagInfos.jets = "ak5PFJetsL2L3Residual"
-newSoftMuonBPFJetNoPUTags = RecoBTag.Configuration.RecoBTag_cff.softMuonBJetTags.clone()
-newSoftMuonBPFJetNoPUTags.tagInfos = cms.VInputTag( cms.InputTag("newPFJetsNoPUSoftMuonTagInfos") )
-newSoftMuonByIP3dBPFJetNoPUTags = RecoBTag.Configuration.RecoBTag_cff.softMuonByIP3dBJetTags.clone()
-newSoftMuonByIP3dBPFJetNoPUTags.tagInfos = cms.VInputTag( cms.InputTag("newPFJetsNoPUSoftMuonTagInfos") )
-newSoftMuonByPtBPFJetNoPUTags = RecoBTag.Configuration.RecoBTag_cff.softMuonByPtBJetTags.clone()
-newSoftMuonByPtBPFJetNoPUTags.tagInfos = cms.VInputTag( cms.InputTag("newPFJetsNoPUSoftMuonTagInfos") )
+newPFNoPUJetsSoftMuonTagInfos = RecoBTag.Configuration.RecoBTag_cff.softMuonTagInfos.clone()
+newPFNoPUJetsSoftMuonTagInfos.jets = "ak5PFJetsL2L3Residual"
+newSoftMuonBPFNoPUJetTags = RecoBTag.Configuration.RecoBTag_cff.softMuonBJetTags.clone()
+newSoftMuonBPFNoPUJetTags.tagInfos = cms.VInputTag( cms.InputTag("newPFNoPUJetsSoftMuonTagInfos") )
+newSoftMuonByIP3dBPFNoPUJetTags = RecoBTag.Configuration.RecoBTag_cff.softMuonByIP3dBJetTags.clone()
+newSoftMuonByIP3dBPFNoPUJetTags.tagInfos = cms.VInputTag( cms.InputTag("newPFNoPUJetsSoftMuonTagInfos") )
+newSoftMuonByPtBPFNoPUJetTags = RecoBTag.Configuration.RecoBTag_cff.softMuonByPtBJetTags.clone()
+newSoftMuonByPtBPFNoPUJetTags.tagInfos = cms.VInputTag( cms.InputTag("newPFNoPUJetsSoftMuonTagInfos") )
 
 # prepare a path running the new modules
-newPFJetNoPUTracksAssociator = cms.Sequence(
-    newPFJetNoPUTracksAssociatorAtVertex
+newPFNoPUJetTracksAssociator = cms.Sequence(
+    newPFNoPUJetTracksAssociatorAtVertex
     )
 
-newPFJetNoPUBtaggingIP = cms.Sequence(
-    newPFJetsNoPUImpactParameterTagInfos * (
-       newTrackCountingHighEffBPFJetNoPUTags +
-       newTrackCountingHighPurBPFJetNoPUTags +
-       newJetProbabilityBPFJetNoPUTags +
-       newJetBProbabilityBPFJetNoPUTags )
+newPFNoPUJetBtaggingIP = cms.Sequence(
+    newPFNoPUJetsImpactParameterTagInfos * (
+       newTrackCountingHighEffBPFNoPUJetTags +
+       newTrackCountingHighPurBPFNoPUJetTags +
+       newJetProbabilityBPFNoPUJetTags +
+       newJetBProbabilityBPFNoPUJetTags )
     )
 
-newPFJetNoPUBtaggingSV = cms.Sequence(
-    newPFJetsNoPUImpactParameterTagInfos *
-    newPFJetsNoPUSecondaryVertexTagInfos * (
-       newSimpleSecondaryVertexHighEffBPFJetNoPUTags +
-       newSimpleSecondaryVertexHighPurBPFJetNoPUTags +
-       newCombinedSecondaryVertexBPFJetNoPUTags +
-       newCombinedSecondaryVertexMVABPFJetNoPUTags )
+newPFNoPUJetBtaggingSV = cms.Sequence(
+    newPFNoPUJetsImpactParameterTagInfos *
+    newPFNoPUJetsSecondaryVertexTagInfos * (
+       newSimpleSecondaryVertexHighEffBPFNoPUJetTags +
+       newSimpleSecondaryVertexHighPurBPFNoPUJetTags +
+       newCombinedSecondaryVertexBPFNoPUJetTags +
+       newCombinedSecondaryVertexMVABPFNoPUJetTags )
     )
 
-newPFJetNoPUBtaggingEle = cms.Sequence(
+newPFNoPUJetBtaggingEle = cms.Sequence(
     softElectronCands * # already run for calojets
-    newPFJetsNoPUSoftElectronTagInfos *
-       newSoftElectronBPFJetNoPUTags +
-       newSoftElectronByIP3dBPFJetNoPUTags +
-       newSoftElectronByPtBPFJetNoPUTags
+    newPFNoPUJetsSoftElectronTagInfos *
+       newSoftElectronBPFNoPUJetTags +
+       newSoftElectronByIP3dBPFNoPUJetTags +
+       newSoftElectronByPtBPFNoPUJetTags
     )
 
-newPFJetNoPUBtaggingMu = cms.Sequence(
-    newPFJetsNoPUSoftMuonTagInfos * (
-       newSoftMuonBPFJetNoPUTags +
-       newSoftMuonByIP3dBPFJetNoPUTags +
-       newSoftMuonByPtBPFJetNoPUTags )
+newPFNoPUJetBtaggingMu = cms.Sequence(
+    newPFNoPUJetsSoftMuonTagInfos * (
+       newSoftMuonBPFNoPUJetTags +
+       newSoftMuonByIP3dBPFNoPUJetTags +
+       newSoftMuonByPtBPFNoPUJetTags )
     )
 
-newPFJetNoPUBtagging = cms.Sequence(
-    newPFJetNoPUBtaggingIP +
-    newPFJetNoPUBtaggingSV +
-    newPFJetNoPUBtaggingEle +
-    newPFJetNoPUBtaggingMu )
+newPFNoPUJetBtagging = cms.Sequence(
+    newPFNoPUJetBtaggingIP +
+    newPFNoPUJetBtaggingSV +
+    newPFNoPUJetBtaggingEle +
+    newPFNoPUJetBtaggingMu )
 
-newPFJetNoPUBtaggingSequence = cms.Sequence(
-    newPFJetNoPUTracksAssociator *
-       newPFJetNoPUBtagging )
+newPFNoPUJetBtaggingSequence = cms.Sequence(
+    newPFNoPUJetTracksAssociator *
+       newPFNoPUJetBtagging )
