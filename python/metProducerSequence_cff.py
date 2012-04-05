081801166fecb9b3926169c19022813250b97db8
emanuele <>
2012-04-05 19:16:04 +0000

    add the corrected pf mets

--- python/metProducerSequence_cff.py
+++ python/metProducerSequence_cff.py
@@ -18,4 +18,18 @@ ourChPFMet = cms.EDProducer("ChargedPFMetProducer",
                             collectionTag = cms.InputTag("reducedPFCands")
                             )
 
-metSequence = cms.Sequence ( reducedPFCands * ourChPFMet * reducedPFCandsToSave )
+from JetMETCorrections.Type1MET.pfMETCorrections_cff import *
+# this does not exist in the release
+pfType0CorrectedMet = pfType1CorrectedMet.clone()
+pfType0CorrectedMet.applyType1Corrections = False
+
+# merge the PF met and the corrected ones into a unique collection of candidates
+pfmets = cms.EDProducer("PfMetCombiner",
+                        labels = cms.VInputTag(cms.InputTag("pfMet"),
+                                               cms.InputTag("pfType0CorrectedMet"),
+                                               cms.InputTag("pfType1CorrectedMet"),
+                                               cms.InputTag("pfType1p2CorrectedMet")
+                                               )
+                        )
+
+metSequence = cms.Sequence ( reducedPFCands * ourChPFMet * reducedPFCandsToSave * pfCandsNotInJet * pfJetMETcorr * pfCandMETcorr * pfchsMETcorr * pfType0CorrectedMet * pfType1CorrectedMet * pfType1p2CorrectedMet * pfmets)
