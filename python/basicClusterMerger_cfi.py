ceeff79f6b026b7098ee52630fc28a2688fa9ee2
emanuele <>
2012-04-23 17:31:53 +0000

    This commit was manufactured by cvs2svn to create tag 'V47-4_2_X'.
    Sprout from master 2012-04-23 17:31:52 UTC emanuele 'backport to 42X'
    Cherrypick from master 2011-04-11 23:04:51 UTC emanuele 'migration to 4_2_0':
        interface/CmsBasicClusterFiller.h
        plugins/LumiAnalyzer.cc
        plugins/LumiAnalyzer.h
        python/basicClusterMerger_cfi.py
        src/CmsBasicClusterFiller.cc
    Cherrypick from master 2012-04-05 19:20:32 UTC emanuele 'modify sequences order to run the corrected PFmets':
        test/python/VecBosAnalysis_MCFastSim_cfg.py
        test/python/VecBosAnalysis_MC_cfg.py
    Cherrypick from master 2012-03-29 18:07:07 UTC emanuele 'further round of code optimization':
        src/CmsMetFiller.cc

--- python/basicClusterMerger_cfi.py
+++ python/basicClusterMerger_cfi.py
@@ -2,5 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 mergedBasicClusters = cms.EDProducer("BasicClusterCombiner",
                                      labels = cms.VInputTag(cms.InputTag("hybridSuperClusters","hybridBarrelBasicClusters"),
-                                                            cms.InputTag("multi5x5SuperClusters","multi5x5EndcapBasicClusters"))
+                                                            cms.InputTag("multi5x5BasicClusters","multi5x5EndcapBasicClusters"))
                                      )
