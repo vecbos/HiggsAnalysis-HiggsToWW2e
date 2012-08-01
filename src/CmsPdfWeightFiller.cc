678a1e270848ef746ab7a07edd31fcef486b33fd
emanuele <>
2012-08-01 14:28:19 +0000

    write the PDFs only if the InputTag is not null

--- src/CmsPdfWeightFiller.cc
+++ src/CmsPdfWeightFiller.cc
@@ -15,15 +15,17 @@ void CmsPdfWeightFiller::writePdfWeightToTree(edm::InputTag pdfSet, const edm::E
 					      const std::string &columnPrefix, const std::string &columnSuffix,
 					      bool dumpData) {
 
-  edm::Handle<std::vector<double> > weightHandle;
-  iEvent.getByLabel(pdfSet, weightHandle);
-
-  std::vector<double> myweights = (*weightHandle);
-
-  std::string nCandString = columnPrefix+(*trkIndexName_)+columnSuffix;
-  cmstree->column(nCandString.c_str(),int(myweights.size()),0,"Reco");
-  cmstree->column((columnPrefix+"w"+columnSuffix).c_str(), myweights, nCandString.c_str(), 0, "Reco");
-
+  std::string label = pdfSet.label();
+  if(label.size()>0) {
+    edm::Handle<std::vector<double> > weightHandle;
+    iEvent.getByLabel(pdfSet, weightHandle);
+    
+    std::vector<double> myweights = (*weightHandle);
+    
+    std::string nCandString = columnPrefix+(*trkIndexName_)+columnSuffix;
+    cmstree->column(nCandString.c_str(),int(myweights.size()),0,"Reco");
+    cmstree->column((columnPrefix+"w"+columnSuffix).c_str(), myweights, nCandString.c_str(), 0, "Reco");
+  }
   if(dumpData) cmstree->dumpData();
 
 }
