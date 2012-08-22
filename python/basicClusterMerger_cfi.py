033188aed595e3189e2f37e1d57a19df68def3ef
emanuele <>
2012-08-22 22:52:40 +0000

    obsolete

--- python/basicClusterMerger_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-mergedBasicClusters = cms.EDProducer("BasicClusterCombiner",
-                                     labels = cms.VInputTag(cms.InputTag("hybridSuperClusters","hybridBarrelBasicClusters"),
-                                                            cms.InputTag("multi5x5SuperClusters","multi5x5EndcapBasicClusters"))
-                                     )
--- python/superClusterMerger_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-mergedSuperClusters = cms.EDProducer("SuperClusterCombiner",
-                                     labels = cms.VInputTag(cms.InputTag("correctedHybridSuperClusters"),
-                                                            cms.InputTag("correctedMulti5x5SuperClustersWithPreshower"))
-                                     )
