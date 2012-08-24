ccde38801fadf68bc541012f21d24fabe3d814ae
emanuele <>
2012-08-24 10:27:39 +0000

    update the MET filters to the ones suggested now

--- python/METOptionalFilterFlags_cff.py
+++ python/METOptionalFilterFlags_cff.py
@@ -1,10 +1,33 @@
 from MyAnalysis.METFlags.EcalDeadCellEventFlagProducer_cfi import *
+
 from MyAnalysis.METFlags.simpleDRFlagProducer_cfi import *
+
 # the CSC Halo filter only works in the RECO
 #from MyAnalysis.METFlags.CSCHaloFlagProducer_cfi import *
-from MyAnalysis.METFlags.trackingFailureFlagProducer_cfi import *
+
+from RecoMET.METFilters.trackingFailureFilter_cfi import *
+trackingFailureFilter.taggingMode = True
+trackingFailureFilter.VertexSource = "goodPrimaryVertices"
+
 from PhysicsTools.EcalAnomalousEventFilter.ecalanomalouseventfilter_cfi import *
 EcalAnomalousEventFilter.recHitsEB = 'reducedEcalRecHitsEB'
 EcalAnomalousEventFilter.recHitsEE = 'reducedEcalRecHitsEE'
 
-metOptionalFilterSequence = cms.Sequence( EcalDeadCellEventFlagProducer * simpleDRFlagProducer * trackingFailureFlagProducer * EcalAnomalousEventFilter)
+from CommonTools.RecoAlgos.HBHENoiseFilterResultProducer_cfi import *
+
+# in 42X, it has to be customized to use run/lumi/event filter, otherwise it needs AOD
+from RecoMET.METFilters.hcalLaserEventFilter_cfi import *
+hcalLaserEventFilter.taggingMode = True
+hcalLaserEventFilter.vetoByRunEventNumber = False
+hcalLaserEventFilter.vetoByHBHEOccupancy= True
+
+from RecoMET.METFilters.eeBadScFilter_cfi import *
+eeBadScFilter.taggingMode = True
+
+metOptionalFilterSequence = cms.Sequence( EcalDeadCellEventFlagProducer
+                                          * HBHENoiseFilterResultProducer
+                                          * hcalLaserEventFilter
+                                          * eeBadScFilter
+                                          * simpleDRFlagProducer
+                                          * trackingFailureFilter
+                                          * EcalAnomalousEventFilter)
