f2e52ca53bb89d91c56ddaebc3491555e1505bcc
mpierini <>
2012-10-01 14:39:04 +0000

    updated FASTSIM cfg file

--- test/python/VecBosAnalysis_MCFastSim_cfg.py
+++ test/python/VecBosAnalysis_MCFastSim_cfg.py
@@ -1,15 +1,23 @@
 import FWCore.ParameterSet.Config as cms
 
+is42X = False
+is52X = True
 runOnAOD = 1
 
 process = cms.Process("VecBosAnalysis")
 
 process.load("Configuration.StandardSequences.MagneticField_38T_cff")
-process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
 process.load("Configuration.StandardSequences.Reconstruction_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'START52_V9::All'
 process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
+
+if(is42X):
+    process.GlobalTag.globaltag = 'START42_V17::All'
+elif(is52X):
+    process.GlobalTag.globaltag = 'START52_V11C::All'
+else:
+    process.GlobalTag.globaltag = 'START53_V10::All'
 
 # --- jet met sequences ---
 process.load("HiggsAnalysis.HiggsToWW2e.metProducerSequence_cff")
@@ -27,23 +35,12 @@ process.load("MyAnalysis.METFlags.logErrorAnalysisProducer_cff")
 
 # do not use residual corrections in MC
 process.load("HiggsAnalysis.HiggsToWW2e.jetProducerSequenceFastJet_cff")
-process.newJetTracksAssociatorAtVertex.jets = 'ak5CaloJetsL1FastL2L3'
-process.newSoftElectronTagInfos.jets = 'ak5CaloJetsL1FastL2L3'
-process.newSoftMuonTagInfos.jets = 'ak5CaloJetsL1FastL2L3'
-process.newPFPUcorrJetTracksAssociatorAtVertex.jets = 'ak5PFJetsL1FastL2L3'
-process.newPFPUcorrJetsSoftElectronTagInfos.jets = 'ak5PFJetsL1FastL2L3'
-process.newPFPUcorrJetsSoftMuonTagInfos.jets = 'ak5PFJetsL1FastL2L3'
-process.newPFNoPUJetTracksAssociatorAtVertex.jets = 'ak5PFNoPUJetsL1FastL2L3'
-process.newPFNoPUJetsSoftElectronTagInfos.jets = 'ak5PFNoPUJetsL1FastL2L3'
-process.newPFNoPUJetsSoftMuonTagInfos.jets = 'ak5PFNoPUJetsL1FastL2L3'
 
-# to correct calo met ---
-#process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
-#from JetMETCorrections.Type1MET.MetType1Corrections_cff import metJESCorAK5CaloJet
-#process.metMuonJESCorAK5 = metJESCorAK5CaloJet.clone()
-#process.metMuonJESCorAK5.inputUncorMetLabel = "met"
-#process.metMuonJESCorAK5.useTypeII = True
-#process.metMuonJESCorAK5.hasMuonsCorr = False
+# process.IgProfService = cms.Service("IgProfService",
+#                                     reportFirstEvent            = cms.untracked.int32(0),
+#                                     reportEventInterval         = cms.untracked.int32(50),
+#                                     reportToFileAtPostEvent     = cms.untracked.string("| gzip -c > YYYY.%I.gz")
+#                                     )
 
 # --- track sequences ---
 process.load("HiggsAnalysis.HiggsToWW2e.leptonLinkedTracks_cfi")
@@ -57,8 +54,12 @@ process.load("HiggsAnalysis.HiggsToWW2e.leptonPFIsoSequence_cff")
 # --- calotowers sequence ---
 process.load("HiggsAnalysis.HiggsToWW2e.lowThrCaloTowers_cfi")
 
-# --- ECAL clusters merging in a unique collection ---
-process.load("HiggsAnalysis.HiggsToWW2e.superClusterMerger_cfi")
+# --- ECAL clusters merging in a unique collection and seeds filtering ---
+process.load("HiggsAnalysis.HiggsToWW2e.electronAndPhotonSuperClusters_cff")
+process.load("HiggsAnalysis.HiggsToWW2e.seedBasicClusters_cff")
+
+# --- good vertex filter ---
+process.load("HiggsAnalysis.HiggsToWW2e.vertexFiltering_cff")
 
 #PDF systematics
 # Produce PDF weights (maximum is 3)
@@ -74,25 +75,24 @@ process.pdfWeights = cms.EDProducer("PdfWeightProducer",
 # --- tree dumper ---
 process.load("HiggsAnalysis.HiggsToWW2e.treeDumper_cfi")
 process.treeDumper.nameFile = 'default_MC.root'
-process.treeDumper.jetCollection1 = 'ak5CaloJetsL1FastL2L3'
-process.treeDumper.jetCollection2 = 'ak5CaloJets::HLT'
-process.treeDumper.JPTjetCollection1 = 'ak5JPTJetsL2L3'
-process.treeDumper.PFjetCollection1 = 'ak5PFNoPUJetsL1FastL2L3'
-process.treeDumper.PFpuCorrJetCollection1 = 'ak5PFJetsL1FastL2L3'
+process.treeDumper.PFJetCorrectionService = 'ak5PFL1FastL2L3'
+process.treeDumper.JetCorrectionService = 'ak5CaloL1FastL2L3'
 process.treeDumper.dumpTriggerResults = True
 process.treeDumper.dumpHLTObjects = True
 process.treeDumper.dumpGenInfo = True
 process.treeDumper.dumpLHE = False
-process.treeDumper.dumpPdfWeight = True
+process.treeDumper.dumpPdfWeight = False
 process.treeDumper.dumpSignalKfactor = False
-process.treeDumper.dumpTracks = True
+process.treeDumper.dumpTracks = False
 process.treeDumper.dumpElectrons = True
 process.treeDumper.dumpGsfTracks = True
 process.treeDumper.dumpSCs = True
-process.treeDumper.dumpBCs = False
+process.treeDumper.dumpBCs = True
+process.treeDumper.dumpConversions = True
 process.treeDumper.dumpVertices = True
 process.treeDumper.dumpCaloTowers = False
 process.treeDumper.dumpGenJets = True
+#process.treeDumper.dumpParticleFlowObjects = False
 process.treeDumper.dumpParticleFlowObjects = True
 process.treeDumper.dumpPFCandidates = False
 process.treeDumper.dumpTree = True
@@ -100,14 +100,27 @@ if (runOnAOD == 1) :
     process.treeDumper.saveFatTrk = False
     process.treeDumper.saveTrackDeDx = False
     process.treeDumper.dumpPFlowElectrons = False
-    process.treeDumper.dumpHcalNoiseFlags = False
-    process.treeDumper.AODHcalNoiseFlags = False
+    process.treeDumper.dumpHcalNoiseFlags = True
+    process.treeDumper.AODHcalNoiseFlags = True
 else :
     process.treeDumper.saveFatTrk = True
     process.treeDumper.saveTrackDeDx = True
     process.treeDumper.dumpPFlowElectrons = True
-    process.treeDumper.dumpHcalNoiseFlags = False
+    process.treeDumper.dumpHcalNoiseFlags = True
     process.treeDumper.AODHcalNoiseFlags = False
+
+# FASTSIM specific
+process.treeDumper.jetCollection2 = cms.InputTag("ak5CaloJets","","HLT")
+process.treeDumper.dumpHcalNoiseFlags = False
+process.treeDumper.AODHcalNoiseFlags = False
+
+# this replaces the collections with existing ones (even if they are saved already)
+if(is42X==True):
+    process.treeDumper.hpsTauDiscrByVLooseCombinedIsolationDBSumPtCorrTag = cms.InputTag("hpsPFTauDiscriminationByVLooseIsolation")
+    process.treeDumper.hpsTauDiscrByLooseCombinedIsolationDBSumPtCorrTag = cms.InputTag("hpsPFTauDiscriminationByLooseIsolation")
+    process.treeDumper.hpsTauDiscrByMediumCombinedIsolationDBSumPtCorrTag = cms.InputTag("hpsPFTauDiscriminationByMediumIsolation")
+    process.treeDumper.hpsTauDiscrByTightCombinedIsolationDBSumPtCorrTag = cms.InputTag("hpsPFTauDiscriminationByTightIsolation")   
+
 
 process.options = cms.untracked.PSet(
       fileMode =  cms.untracked.string('NOMERGE')
@@ -115,40 +128,55 @@ process.options = cms.untracked.PSet(
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
+#if(is42X):
+#    inputfile = cms.untracked.vstring('file:/cmsrm/pc24_2/emanuele/data/DYeeSummer11.root')
+#elif(is52X):
+#    inputfile = cms.untracked.vstring('file:/cmsrm/pc21_2/emanuele/data/AOD_WWSummer12.root')
+#else:
+#    inputfile = cms.untracked.vstring('/store/mc/Summer12_DR53X/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball/AODSIM/PU_S10_START53_V7A-v1/0000/FEFF0D8E-91D2-E111-9BEA-001E673976ED.root')
+inputfile = cms.untracked.vstring('/store/mc/Summer12/SMS-T2tt_FineBin_Mstop-225to1200_mLSP-0to1000_8TeV-Pythia6Z/AODSIM/S\
+TART52_V9_FSIM-v1/00003/FC2E3270-D7E0-E111-B1E2-848F69FD4565.root')
+
 process.source = cms.Source("PoolSource",
                             noEventSort = cms.untracked.bool(True),
                             duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
-#                            fileNames = cms.untracked.vstring('file:/cmsrm/pc23/emanuele/data/Pool/jpsiEE_Fall10.root') # RECO
-#                            fileNames = cms.untracked.vstring('file:/cmsrm/pc23_2/emanuele/Pool/AODSIM_Winter10_FlatPU.root')
-                            fileNames = cms.untracked.vstring('file:AODFASTSIM.root')
+                            fileNames = inputfile
                             )
 
+process.prejets = cms.Sequence( process.leptonLinkedTracks 
+                                * process.electronAndPhotonSuperClustersSequence
+                                * process.chargedMetProducer
+                                * process.pfIsolationAllSequence )
+
+if(process.treeDumper.dumpBCs==True):
+    process.prejets *= process.seedBasicClustersSequence 
+
+process.jets = cms.Sequence( process.ourJetSequenceMCReduced
+                             * process.newBtaggingSequence 
+                             * process.newPFPUcorrJetBtaggingSequence
+                             * process.newPFNoPUJetBtaggingSequence
+                             * process.metSequence )
+
+process.postjets = cms.Sequence( process.eIdSequence
+                                 * process.FastjetForIsolation
+                                 * process.treeDumper )
+
+# In order to use the good primary vertices everywhere (It would be nicer to set the proper inputTags in the first place)
+from PhysicsTools.PatAlgos.tools.helpers import *
+massSearchReplaceAnyInputTag(process.prejets,cms.InputTag("offlinePrimaryVertices"), cms.InputTag("goodPrimaryVertices"),True)
+massSearchReplaceAnyInputTag(process.jets,cms.InputTag("offlinePrimaryVertices"), cms.InputTag("goodPrimaryVertices"),True)
+massSearchReplaceAnyInputTag(process.postjets,cms.InputTag("offlinePrimaryVertices"), cms.InputTag("goodPrimaryVertices"),True)
+
 if(process.treeDumper.dumpPdfWeight == False) :
-    process.p = cms.Path ( process.leptonLinkedTracks
-                       * process.mergedSuperClusters
-                       * process.chargedMetProducer
-                       * process.pfIsolationAllSequence
-                       * process.ourJetSequenceMCReduced
-                       * process.newBtaggingSequence
-                       * process.newPFPUcorrJetBtaggingSequence
-                       * process.newPFNoPUJetBtaggingSequence
-                       * process.metSequence
-                       * process.eIdSequence
-                       * process.FastjetForIsolation
-                       * process.treeDumper
-                       )
+    process.p = cms.Path ( process.goodPrimaryVertices
+                           * process.prejets
+                           * process.jets
+                           * process.postjets
+                           )
 else :
     process.p = cms.Path ( process.pdfWeights
-                           * process.leptonLinkedTracks
-                           * process.mergedSuperClusters
-                           * process.chargedMetProducer
-                           * process.pfIsolationAllSequence
-                           * process.ourJetSequenceMCReduced
-                           * process.newBtaggingSequence 
-                           * process.newPFPUcorrJetBtaggingSequence
-                           * process.newPFNoPUJetBtaggingSequence
-                           * process.metSequence
-                           * process.eIdSequence
-                           * process.FastjetForIsolation
-                           * process.treeDumper
+                           * process.goodPrimaryVertices
+                           * process.prejets
+                           * process.jets
+                           * process.postjets
                            )
